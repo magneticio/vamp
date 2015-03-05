81bfbe3ef8aa3c14a905fe75375712c9cad139eb
plamola <github@plamola.com>
2015-03-05 20:23:45 +0100

    Removed unused imports

--- src/main/scala/io/magnetic/vamp_core/persistence/slick/operations/actor/Dispatch.scala
+++ src/main/scala/io/magnetic/vamp_core/persistence/slick/operations/actor/Dispatch.scala
@@ -1,15 +1,10 @@
 package io.magnetic.vamp_core.persistence.slick.operations.actor
 
 import io.magnetic.vamp_core.persistence.common.operations.message.Messages
-import Messages.{DispatchError, BreedOps, SaveBreed}
-import io.magnetic.vamp_core.persistence.common.operations.message._
+import Messages.BreedOps
 import io.magnetic.vamp_core.persistence.common.operations.message.Messages.DispatchError
 import io.magnetic.vamp_core.persistence.slick.model.Schema
-import scala.slick.driver.JdbcDriver.simple._
-
-
 import akka.actor.{Props, Actor}
-import akka.actor.Actor.Receive
 
 
 class Dispatch(val schema: Schema) extends Actor{
