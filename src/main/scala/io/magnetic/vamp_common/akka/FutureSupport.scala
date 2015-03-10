b6303ec4a6d772a55faaa1a844e0c540d1cbc8fc
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2015-03-10 15:24:25 +0100

    future support notifications

--- src/main/scala/io/magnetic/vamp_common/akka/FutureSupport.scala
+++ src/main/scala/io/magnetic/vamp_common/akka/FutureSupport.scala
@@ -1,6 +1,7 @@
 package io.magnetic.vamp_common.akka
 
 import akka.util.Timeout
+import io.magnetic.vamp_common.notification.{Notification, NotificationProvider}
 
 import scala.concurrent._
 import scala.util.{Failure, Success}
@@ -24,4 +25,18 @@ trait FutureSupport {
         } yield previousResults :+ next
     }
   }
+}
+
+trait FutureSupportNotification extends FutureSupport {
+  this: NotificationProvider =>
+
+  def offLoad(future: Future[Any], `class`: Class[_ <: Notification])(implicit timeout: Timeout): Any = {
+    Await.ready(future, timeout.duration)
+    future.value.get match {
+      case Success(result) => result
+      case Failure(failure) => exceptionNotification(failure, `class`)
+    }
+  }
+
+  def exceptionNotification(failure: AnyRef, `class`: Class[_ <: Notification]) = exception(`class`.getConstructors()(0).newInstance(failure).asInstanceOf[Notification])
 }
