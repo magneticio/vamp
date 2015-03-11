c4d822f6fc89090751b10a64be176aad0617f2cd
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2015-03-11 11:14:55 +0100

    #177 generic error notification with exeption

--- container_driver/src/main/scala/io/magnetic/vamp_core/container_driver/notification/ContainerDriverNotification.scala
+++ container_driver/src/main/scala/io/magnetic/vamp_core/container_driver/notification/ContainerDriverNotification.scala
@@ -1,10 +1,10 @@
 package io.magnetic.vamp_core.container_driver.notification
 
 import io.magnetic.vamp_common.akka.RequestError
-import io.magnetic.vamp_common.notification.Notification
+import io.magnetic.vamp_common.notification.{ErrorNotification, Notification}
 
 case class UnsupportedContainerDriverError(name: String) extends Notification
 
 case class UnsupportedContainerDriverRequest(request: Any) extends Notification with RequestError
 
-case class ContainerResponseError(any: Any) extends Notification
+case class ContainerResponseError(reason: Any) extends Notification with ErrorNotification
--- router_driver/src/main/scala/io/magnetic/vamp_core/router_driver/notification/RouterDriverNotification.scala
+++ router_driver/src/main/scala/io/magnetic/vamp_core/router_driver/notification/RouterDriverNotification.scala
@@ -1,8 +1,8 @@
 package io.magnetic.vamp_core.router_driver.notification
 
 import io.magnetic.vamp_common.akka.RequestError
-import io.magnetic.vamp_common.notification.Notification
+import io.magnetic.vamp_common.notification.{ErrorNotification, Notification}
 
 case class UnsupportedRouterDriverRequest(request: Any) extends Notification with RequestError
 
-case class RouterResponseError(any: Any) extends Notification
+case class RouterResponseError(reason: Any) extends Notification  with ErrorNotification
