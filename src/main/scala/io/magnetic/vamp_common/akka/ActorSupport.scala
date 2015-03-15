82c2f8e880141f2ea8cd46cfe65fe56d86c9c108
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2015-03-15 20:03:54 +0100

    akka mailbox support

--- src/main/scala/io/magnetic/vamp_common/akka/ActorSupport.scala
+++ src/main/scala/io/magnetic/vamp_common/akka/ActorSupport.scala
@@ -10,13 +10,15 @@ trait ActorDescription {
 }
 
 object ActorSupport {
-  def actorOf(actorDescription: ActorDescription, args: Any*)(implicit actorSystem: ActorSystem) = actorSystem.actorOf(actorDescription.props(args: _*), actorDescription.name)
+  def actorOf(actorDescription: ActorDescription, args: Any*)(implicit  mailbox: String = "akka.actor.default-mailbox", actorSystem: ActorSystem) =
+    actorSystem.actorOf(actorDescription.props(args: _*).withMailbox(mailbox), actorDescription.name)
 }
 
 trait ActorSupport {
   this: Actor =>
 
-  def actorOf(actorDescription: ActorDescription, args: Any*) = context.system.actorOf(actorDescription.props(args: _*), actorDescription.name)
+  def actorOf(actorDescription: ActorDescription, args: Any*)(implicit mailbox: String = "akka.actor.default-mailbox") =
+    context.system.actorOf(actorDescription.props(args: _*).withMailbox(mailbox), actorDescription.name)
 
   def actorFor(actorDescription: ActorDescription) = context.actorSelection(s"../${actorDescription.name}")
 }
