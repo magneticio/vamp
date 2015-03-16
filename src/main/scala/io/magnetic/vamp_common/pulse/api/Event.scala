6e26f6d562b5272db2563b14d30f252571c8dde8
Roman Useinov <roman.useinov@gmail.com>
2015-03-16 13:06:02 +0100

    changed a default, we can actually remove it, but it's there for convenience, so that you won't have to supply it everytime you need to aggregate a simple numeric value

--- src/main/scala/io/magnetic/vamp_common/pulse/api/Event.scala
+++ src/main/scala/io/magnetic/vamp_common/pulse/api/Event.scala
@@ -10,4 +10,4 @@ import java.time.temporal.ChronoUnit
 case class Event(tags: List[String], value: AnyRef, timestamp: OffsetDateTime = OffsetDateTime.now(), `type`: String = "")
 case class EventQuery(tags: List[String] = List.empty, time: TimeRange = TimeRange(), aggregator: Option[Aggregator] = Option.empty, `type`: String = "metric")
 case class TimeRange(from: OffsetDateTime = OffsetDateTime.now().minus(100, ChronoUnit.MINUTES), to: OffsetDateTime = OffsetDateTime.now())
-case class Aggregator(`type`: String, field: String = "value")
+case class Aggregator(`type`: String, field: String = "numeric")
