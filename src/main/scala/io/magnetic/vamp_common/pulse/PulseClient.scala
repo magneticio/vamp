31ad5cec36938bf1f272bf261e6efc1a51e63592
Roman Useinov <roman.useinov@gmail.com>
2015-03-17 16:04:04 +0100

    changed pulse client

--- src/main/scala/io/magnetic/vamp_common/pulse/PulseClient.scala
+++ src/main/scala/io/magnetic/vamp_common/pulse/PulseClient.scala
@@ -11,6 +11,6 @@ import scala.concurrent.Future
 
 
 class PulseClient(url: String)(implicit val formats: Formats = Serializers.formats) {
-  def sendEvent(event: Event): Future[Any] = RestClient.request[Any](s"POST $url/api/v1/events", event)
+  def sendEvent(event: Event): Future[Event] = RestClient.request[Event](s"POST $url/api/v1/events", event)
   def getEvents(eventQuery: EventQuery): Future[Any] = RestClient.request[Any](s"POST $url/api/v1/events/get", eventQuery)
 }
