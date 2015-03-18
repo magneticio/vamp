daa874e1534858b743fa0c9e4c4f3e67d69d1f20
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2015-03-18 10:57:23 +0100

    double creation by deafult is allowed (rest api)

--- persistence/src/main/scala/io/magnetic/vamp_core/persistence/store/InMemoryStoreProvider.scala
+++ persistence/src/main/scala/io/magnetic/vamp_core/persistence/store/InMemoryStoreProvider.scala
@@ -36,7 +36,7 @@ trait InMemoryStoreProvider extends StoreProvider with PersistenceNotificationPr
     }
 
 
-    def create(artifact: Artifact, ignoreIfExists: Boolean = false): Artifact = {
+    def create(artifact: Artifact, ignoreIfExists: Boolean = true): Artifact = {
       logger.trace(s"persistence create [${artifact.getClass.getSimpleName}] - ${write(artifact)}")
       getBranch(artifact) match {
         case Some(branch) => store.get(branch) match {
