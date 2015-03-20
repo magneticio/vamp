fa339af2d7dd8e342d6801d4b77c5d32a9353454
Roman Useinov <roman.useinov@gmail.com>
2015-03-20 12:22:11 +0100

    maven requirements

---
+++ javadoc/readme.md
@@ -0,0 +1,5 @@
+Vamp Common
+===============================
+
+Common is a set of libraries that are used throughout vamp projects,
+such as notification system, various traits, helpers and such.
--- src/main/scala/io/vamp/common/pulse/PulseClient.scala
+++ src/main/scala/io/vamp/common/pulse/PulseClient.scala
@@ -13,4 +13,5 @@ import scala.concurrent.Future
 class PulseClient(url: String)(implicit val formats: Formats = Serializers.formats) {
   def sendEvent(event: Event): Future[Event] = RestClient.request[Event](s"POST $url/api/v1/events", event)
   def getEvents(eventQuery: EventQuery): Future[Any] = RestClient.request[Any](s"POST $url/api/v1/events/get", eventQuery)
+  def resetEvents(): Future[Any] = RestClient.request[Any](s"GET $url/api/v1/events/reset")
 }
