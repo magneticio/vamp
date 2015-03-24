b3d8adc4d37659d1790b3c81b797b4fe13c0889e
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2015-03-24 12:09:21 +0100

    pulse event

--- src/main/scala/io/vamp/common/notification/Notification.scala
+++ src/main/scala/io/vamp/common/notification/Notification.scala
@@ -6,4 +6,10 @@ trait ErrorNotification extends Notification {
   def reason: Any
 }
 
+trait PulseEvent {
+  def tags: List[String] = Nil
+
+  def schema: String = ""
+}
+
 case class NotificationErrorException(notification: Notification, message: String) extends RuntimeException(message)
