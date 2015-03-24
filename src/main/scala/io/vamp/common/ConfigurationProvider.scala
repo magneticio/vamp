9a4dab69388aa8f94d779f816d057b44fe4c92a4
Roman Useinov <roman.useinov@gmail.com>
2015-03-24 12:25:32 +0100

    configuration provider

---
+++ src/main/scala/io/vamp/common/ConfigurationProvider.scala
@@ -0,0 +1,13 @@
+package io.vamp.common
+
+import com.typesafe.config.ConfigFactory
+
+
+trait ConfigurationProvider {
+  protected val confPath: String
+  protected val config = ConfigFactory.load().getConfig(confPath)
+}
+
+trait DefaultConfigurationProvider extends ConfigurationProvider {
+  override protected val confPath: String = ""
+}
