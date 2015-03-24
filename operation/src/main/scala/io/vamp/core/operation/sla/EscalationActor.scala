08e009bbe756df8cb58785fe78324ef9a16d0161
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2015-03-24 12:05:00 +0100

    to all & to one escalation

--- operation/src/main/scala/io/vamp/core/operation/sla/EscalationActor.scala
+++ operation/src/main/scala/io/vamp/core/operation/sla/EscalationActor.scala
@@ -77,8 +77,8 @@ class EscalationActor extends Actor with ActorLogging with ActorSupport with Fut
             implicit val timeout = PulseDriverActor.timeout
             offLoad(actorFor(PulseDriverActor) ? PulseDriverActor.QuerySlaEvents(deployment, cluster, from, to)) match {
               case escalationEvents: List[_] => escalationEvents.foreach {
-                case Escalate(d, c) if d.name == deployment.name && c.name == cluster.name => escalate(deployment, cluster, sla, escalate = true)
-                case DeEscalate(d, c) if d.name == deployment.name && c.name == cluster.name => escalate(deployment, cluster, sla, escalate = false)
+                case Escalate(d, c) if d.name == deployment.name && c.name == cluster.name => escalateToAll(deployment, cluster, sla.escalations, escalate = true)
+                case DeEscalate(d, c) if d.name == deployment.name && c.name == cluster.name => escalateToAll(deployment, cluster, sla.escalations, escalate = false)
                 case _ =>
               }
               case any => exception(InternalServerError(any))
@@ -88,17 +88,30 @@ class EscalationActor extends Actor with ActorLogging with ActorSupport with Fut
     })
   }
 
-  private def escalate(deployment: Deployment, cluster: DeploymentCluster, sla: Sla, escalate: Boolean) = {
-    sla.escalations.foreach {
-      case e: ScaleEscalation[_] => scaleEscalation(deployment, cluster, e, escalate)
-      case e: ToAllEscalation => // TODO
-      case e: ToOneEscalation => // TODO
-      case e: GenericEscalation => info(UnsupportedEscalationType(e.`type`))
-      case e: Escalation => error(UnsupportedEscalationType(e.name))
-    }
+  private def escalateToAll(deployment: Deployment, cluster: DeploymentCluster, escalations: List[Escalation], escalate: Boolean): Boolean = {
+    escalations.foldLeft(false)((result, escalation) => result || (escalation match {
+      case e: ScaleEscalation[_] =>
+        scaleEscalation(deployment, cluster, e, escalate)
+
+      case e: ToAllEscalation =>
+        escalateToAll(deployment, cluster, e.escalations, escalate)
+
+      case e: ToOneEscalation => e.escalations.find(escalation => escalateToAll(deployment, cluster, escalation :: Nil, escalate)) match {
+        case None => false
+        case Some(_) => true
+      }
+
+      case e: GenericEscalation =>
+        info(UnsupportedEscalationType(e.`type`))
+        false
+
+      case e: Escalation =>
+        error(UnsupportedEscalationType(e.name))
+        false
+    }))
   }
 
-  private def scaleEscalation(deployment: Deployment, cluster: DeploymentCluster, escalation: ScaleEscalation[_], escalate: Boolean) = {
+  private def scaleEscalation(deployment: Deployment, cluster: DeploymentCluster, escalation: ScaleEscalation[_], escalate: Boolean): Boolean = {
     (escalation.targetCluster match {
       case None => Some(cluster)
       case Some(name) => deployment.clusters.find(_.name == name) match {
@@ -106,22 +119,31 @@ class EscalationActor extends Actor with ActorLogging with ActorSupport with Fut
         case Some(c) => Some(c)
       }
     }) match {
-      case None =>
+      case None => false
       case Some(targetCluster) =>
         // Scale only the first service.
         val scale = cluster.services.head.scale.get
         escalation match {
           case ScaleInstancesEscalation(_, minimum, maximum, scaleBy, _) =>
             val instances = if (escalate) scale.instances + scaleBy else scale.instances - scaleBy
-            if (instances <= maximum && instances >= minimum) commit(targetCluster, scale.copy(instances = instances.toInt))
+            if (instances <= maximum && instances >= minimum) {
+              commit(targetCluster, scale.copy(instances = instances.toInt))
+              true
+            } else false
 
           case ScaleCpuEscalation(_, minimum, maximum, scaleBy, _) =>
             val cpu = if (escalate) scale.cpu + scaleBy else scale.cpu - scaleBy
-            if (cpu <= maximum && cpu >= minimum) commit(targetCluster, scale.copy(cpu = cpu))
+            if (cpu <= maximum && cpu >= minimum) {
+              commit(targetCluster, scale.copy(cpu = cpu))
+              true
+            } else false
 
           case ScaleMemoryEscalation(_, minimum, maximum, scaleBy, _) =>
             val memory = if (escalate) scale.memory + scaleBy else scale.memory - scaleBy
-            if (memory <= maximum && memory >= minimum) commit(targetCluster, scale.copy(memory = memory))
+            if (memory <= maximum && memory >= minimum) {
+              commit(targetCluster, scale.copy(memory = memory))
+              true
+            } else false
         }
     }
 
