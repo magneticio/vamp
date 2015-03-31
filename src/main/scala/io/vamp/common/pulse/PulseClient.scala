2a8a225cf8bd6ac50973364ad302b41dc59fafc1
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2015-03-31 10:46:26 +0200

    additional methods in pulse client

--- src/main/scala/io/vamp/common/pulse/PulseClient.scala
+++ src/main/scala/io/vamp/common/pulse/PulseClient.scala
@@ -12,73 +12,77 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 
-
 class PulseClient(val url: String)(implicit val formats: Formats = Serializers.formats) {
+
   import PulseClient._
+
   def sendEvent(event: Event): Future[Event] = RestClient.request[Event](s"POST $url/api/v1/events", event)
-  def getEvents(tags: List[String] = List.empty, `type`: String = "", from: Option[OffsetDateTime] = None, to: Option[OffsetDateTime] = None): Future[List[Event]] =
-    RestClient.request[List[Event]](s"POST $url/api/v1/events/get", constructQuery(tags = tags, `type` = `type`, from = from, to = to))
-  def resetEvents(): Future[Any] = RestClient.request[Any](s"GET $url/api/v1/events/reset")
 
+  def getEvents(tags: List[String], from: OffsetDateTime, to: OffsetDateTime): Future[List[Event]] = getEvents(tags, Some(from), Some(to))
 
+  def getEvents(tags: List[String], from: Option[OffsetDateTime] = None, to: Option[OffsetDateTime] = None, `type`: String = ""): Future[List[Event]] =
+    RestClient.request[List[Event]](s"POST $url/api/v1/events/get", constructQuery(tags, from, to, `type`, aggregator = None))
 }
 
 object PulseClient {
-  def constructQuery(tags: List[String] = List.empty, `type`: String = "", from: Option[OffsetDateTime] = None, to: Option[OffsetDateTime] = None, aggregator: Option[Aggregator] = None): EventQuery = {
+
+  def constructQuery(tags: List[String], from: Option[OffsetDateTime], to: Option[OffsetDateTime], `type`: String, aggregator: Option[Aggregator]): EventQuery = {
     var timeRange = TimeRange()
-    if(from.isDefined) timeRange = timeRange.copy(from = from.get)
-    if(to.isDefined) timeRange = timeRange.copy(to = to.get)
-    
+    if (from.isDefined) timeRange = timeRange.copy(from = from.get)
+    if (to.isDefined) timeRange = timeRange.copy(to = to.get)
+
     var eventQuery = EventQuery(tags = tags, time = timeRange, `type` = `type`)
-    
-    if(aggregator.isDefined) eventQuery = eventQuery.copy(aggregator = aggregator)
-    
+
+    if (aggregator.isDefined) eventQuery = eventQuery.copy(aggregator = aggregator)
+
     eventQuery
   }
 }
-
-
-
-
 
 
 case class SimpleAggregationResult(value: Double)
 
 trait SimpleAggregation {
   this: PulseClient =>
+
   import PulseClient._
 
-  protected def getAggregation(tags: List[String], `type`: String, from: Option[OffsetDateTime], to: Option[OffsetDateTime], field: String, aggregatorType: AggregatorType): Future[SimpleAggregationResult] = {
+  protected def getAggregation(tags: List[String], from: Option[OffsetDateTime], to: Option[OffsetDateTime], `type`: String, field: String, aggregatorType: AggregatorType): Future[SimpleAggregationResult] = {
     RestClient.request[SimpleAggregationResult](s"POST $url/api/v1/events/get", constructQuery(tags = tags, `type` = `type`, from = from, to = to, aggregator = Some(Aggregator(aggregatorType, field))))
   }
 }
 
 
-
-
-
 trait Count extends SimpleAggregation {
   this: PulseClient =>
 
-  def count(tags: List[String] = List.empty, `type`: String = "", from: Option[OffsetDateTime] = None, to: Option[OffsetDateTime] = None, field: String = "") = getAggregation(tags, `type`, from, to, field, AggregatorType.count)
+  def count(tags: List[String], from: OffsetDateTime, to: OffsetDateTime): Future[SimpleAggregationResult] = count(tags, Some(from), Some(to))
+
+  def count(tags: List[String], from: Option[OffsetDateTime] = None, to: Option[OffsetDateTime] = None, `type`: String = "", field: String = ""): Future[SimpleAggregationResult] = getAggregation(tags, from, to, `type`, field, AggregatorType.count)
 }
 
 trait Max extends SimpleAggregation {
   this: PulseClient =>
 
-  def max(tags: List[String] = List.empty, `type`: String = "", from: Option[OffsetDateTime] = None, to: Option[OffsetDateTime] = None, field: String = "") = getAggregation(tags, `type`, from, to, field, AggregatorType.max)
+  def max(tags: List[String], from: OffsetDateTime, to: OffsetDateTime): Future[SimpleAggregationResult] = max(tags, Some(from), Some(to))
+
+  def max(tags: List[String], from: Option[OffsetDateTime] = None, to: Option[OffsetDateTime] = None, `type`: String = "", field: String = ""): Future[SimpleAggregationResult] = getAggregation(tags, from, to, `type`, field, AggregatorType.max)
 }
 
 trait Min extends SimpleAggregation {
   this: PulseClient =>
 
-  def min(tags: List[String] = List.empty, `type`: String = "", from: Option[OffsetDateTime] = None, to: Option[OffsetDateTime] = None, field: String = "") = getAggregation(tags, `type`, from, to, field, AggregatorType.min)
+  def min(tags: List[String], from: OffsetDateTime, to: OffsetDateTime): Future[SimpleAggregationResult] = min(tags, Some(from), Some(to))
+
+  def min(tags: List[String], from: Option[OffsetDateTime] = None, to: Option[OffsetDateTime] = None, `type`: String = "", field: String = ""): Future[SimpleAggregationResult] = getAggregation(tags, from, to, `type`, field, AggregatorType.min)
 }
 
 trait Average extends SimpleAggregation {
   this: PulseClient =>
 
-  def average(tags: List[String] = List.empty, `type`: String = "", from: Option[OffsetDateTime] = None, to: Option[OffsetDateTime] = None, field: String = "") = getAggregation(tags, `type`, from, to, field, AggregatorType.average)
+  def average(tags: List[String], from: OffsetDateTime, to: OffsetDateTime): Future[SimpleAggregationResult] = average(tags, Some(from), Some(to))
+
+  def average(tags: List[String], from: Option[OffsetDateTime] = None, to: Option[OffsetDateTime] = None, `type`: String = "", field: String = ""): Future[SimpleAggregationResult] = getAggregation(tags, from, to, `type`, field, AggregatorType.average)
 }
 
 
