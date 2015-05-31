8c4b2f42d666973aeffd1518a041aeefda5c498b
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2015-05-31 19:21:45 +0200

    time serializer: added to unix micro time

--- src/main/scala/io/vamp/common/json/OffsetDateTimeSerializer.scala
+++ src/main/scala/io/vamp/common/json/OffsetDateTimeSerializer.scala
@@ -8,11 +8,19 @@ import org.json4s._
 
 object OffsetDateTimeSerializer extends SerializationFormat {
   override def customSerializers = super.customSerializers :+ new OffsetDateTimeSerializer()
+
+  def toUnixMicro(dateTime: OffsetDateTime): Double = {
+    val time = dateTime.toEpochSecond + (dateTime.getNano / 1E+09)
+    (math rint time * 1E+06) / 1E+06
+  }
 }
 
 class OffsetDateTimeSerializer extends CustomSerializer[OffsetDateTime](format => ( {
   case JString(string) => OffsetDateTime.parse(string)
-  case JDouble(number) => OffsetDateTime.from(Instant.ofEpochSecond(number.toLong).atZone(ZoneId.of("UTC")))
+  case JDouble(number) =>
+    val epoch = OffsetDateTime.from(Instant.ofEpochSecond(number.toLong).atZone(ZoneId.of("UTC")))
+    val micro = ((number % 1) * 1E+06).toLong
+    epoch.plusNanos((micro * 1E+03).toLong)
 }, {
   case dateTime: OffsetDateTime => JString(dateTime.format(ISO_INSTANT))
 }))
