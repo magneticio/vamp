d4b29d52205f4c3d7ca2622e3ccf99de665ec3d8
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2015-06-16 16:53:48 +0200

    bug fix: shceduler actor with tick message and not with direct method call

--- src/main/scala/io/vamp/common/akka/SchedulerActor.scala
+++ src/main/scala/io/vamp/common/akka/SchedulerActor.scala
@@ -1,7 +1,7 @@
 package io.vamp.common.akka
 
 import akka.actor._
-import io.vamp.common.akka.SchedulerActor.Period
+import io.vamp.common.akka.SchedulerActor.{Period, Tick}
 
 import scala.concurrent.duration._
 import scala.language.postfixOps
@@ -10,6 +10,8 @@ object SchedulerActor {
 
   case class Period(period: FiniteDuration)
 
+  object Tick
+
 }
 
 abstract class SchedulerActor extends CommonSupportForActors {
@@ -17,18 +19,13 @@ abstract class SchedulerActor extends CommonSupportForActors {
   private var timer: Option[Cancellable] = None
 
   def receive: Receive = {
+    case Tick => tick()
     case Period(period) => schedule(period)
   }
 
   def schedule(period: FiniteDuration) = {
     timer.map(_.cancel())
-    if (period.toNanos > 0) {
-      timer = Some(context.system.scheduler.schedule(0 seconds, period, new Runnable {
-        def run() = {
-          tick()
-        }
-      }))
-    } else timer = None
+    timer = if (period.toNanos > 0) Some(context.system.scheduler.schedule(0 seconds, period, self, Tick)) else None
   }
 
   def tick(): Unit
