9c9fdc57945735d2b0be76f2155535864d4fc537
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2015-06-16 16:19:46 +0200

    workflow serialization format

--- src/main/scala/io/vamp/core/persistence/actor/ArchivingProvider.scala
+++ src/main/scala/io/vamp/core/persistence/actor/ArchivingProvider.scala
@@ -5,6 +5,7 @@ import com.typesafe.config.ConfigFactory
 import io.vamp.common.akka.ExecutionContextProvider
 import io.vamp.common.notification.NotificationProvider
 import io.vamp.core.model.artifact._
+import io.vamp.core.model.workflow.{ScheduledWorkflow, Workflow}
 import io.vamp.core.persistence.notification.ArtifactArchivingError
 import io.vamp.pulse.client.PulseClientProvider
 import io.vamp.pulse.model.Event
@@ -41,6 +42,8 @@ trait ArchivingProvider extends PulseClientProvider {
     case t if classOf[Escalation].isAssignableFrom(t) => Some(s"escalations:${artifact.name}")
     case t if classOf[Routing].isAssignableFrom(t) => Some(s"routings:${artifact.name}")
     case t if classOf[Filter].isAssignableFrom(t) => Some(s"filters:${artifact.name}")
+    case t if classOf[Workflow].isAssignableFrom(t) => Some(s"workflows:${artifact.name}")
+    case t if classOf[ScheduledWorkflow].isAssignableFrom(t) => Some(s"scheduled-workflows:${artifact.name}")
     case request => None
   }
 }
