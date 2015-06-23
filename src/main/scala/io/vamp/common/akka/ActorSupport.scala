f9d907c88fbee1c104655b4cf4a7e93132cc8230
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2015-06-23 15:28:27 +0200

    actor support aliases

--- src/main/scala/io/vamp/common/akka/ActorSupport.scala
+++ src/main/scala/io/vamp/common/akka/ActorSupport.scala
@@ -3,6 +3,7 @@ package io.vamp.common.akka
 import akka.actor._
 import io.vamp.common.text.Text
 
+import scala.collection.mutable
 
 trait ActorDescription {
   def props(args: Any*): Props
@@ -11,21 +12,27 @@ trait ActorDescription {
 }
 
 object ActorSupport {
-  def actorOf(actorDescription: ActorDescription, args: Any*)(implicit mailbox: String = "akka.actor.default-mailbox", actorSystem: ActorSystem) =
-    actorSystem.actorOf(actorDescription.props(args: _*).withMailbox(mailbox), actorDescription.name)
 
-  def actorFor(actorDescription: ActorDescription)(implicit actorSystem: ActorSystem) =
-    actorSystem.actorSelection(s"/user/${actorDescription.name}")
+  private val aliases: mutable.Map[String, ActorDescription] = mutable.Map()
+
+  def alias(from: ActorDescription, to: ActorDescription) = aliases.put(from.name, to)
+
+  def actorOf(actorDescription: ActorDescription, args: Any*)(implicit mailbox: String = "akka.actor.default-mailbox", actorRefFactory: ActorRefFactory) = {
+    val description = aliases.getOrElse(actorDescription.name, actorDescription)
+    actorRefFactory.actorOf(description.props(args: _*).withMailbox(mailbox), description.name)
+  }
+
+  def actorFor(actorDescription: ActorDescription)(implicit actorRefFactory: ActorRefFactory) =
+    actorRefFactory.actorSelection(s"/user/${actorDescription.name}")
 }
 
 trait ActorSupport {
 
   implicit def actorRefFactory: ActorRefFactory
 
-  def actorOf(actorDescription: ActorDescription, args: Any*)(implicit mailbox: String = "akka.actor.default-mailbox") =
-    actorRefFactory.actorOf(actorDescription.props(args: _*).withMailbox(mailbox), actorDescription.name)
+  def actorOf(actorDescription: ActorDescription, args: Any*)(implicit mailbox: String = "akka.actor.default-mailbox") = ActorSupport.actorOf(actorDescription, args)
 
-  def actorFor(actorDescription: ActorDescription) = actorRefFactory.actorSelection(s"/user/${actorDescription.name}")
+  def actorFor(actorDescription: ActorDescription) = ActorSupport.actorFor(actorDescription)
 }
 
 trait ActorSupportForActors extends ActorSupport {
