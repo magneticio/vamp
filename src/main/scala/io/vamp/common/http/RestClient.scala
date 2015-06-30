92f0ed040d91f033f1bdf26df342226184eb4bb4
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2015-06-30 13:36:42 +0200

    rest client: support for option[] body

--- src/main/scala/io/vamp/common/http/RestClient.scala
+++ src/main/scala/io/vamp/common/http/RestClient.scala
@@ -1,6 +1,5 @@
 package io.vamp.common.http
 
-import _root_.io.vamp.common.text.Text
 import com.typesafe.scalalogging.Logger
 import dispatch._
 import org.json4s._
@@ -47,19 +46,11 @@ object RestClient {
 
     val requestWithUrl = dispatch.url(url).setMethod(method.toString)
     val requestWithHeaders = headers.foldLeft(requestWithUrl)((http, header) => http.setHeader(header._1, header._2))
-    val requestWithBody = body match {
-      case str: String =>
-        logger.trace(s"req [${method.toString} $url] - $str")
-        requestWithHeaders.setBody(str)
-      case any: AnyRef if any != null && any != None =>
-        val request = write(any)
-        logger.trace(s"req [${method.toString} $url] - $request")
-        requestWithHeaders.setBody(request)
-      case any if any != null && any != None =>
-        val request = any.toString
-        logger.trace(s"req [${method.toString} $url] - $request")
-        requestWithHeaders.setBody(request)
-      case _ =>
+    val requestWithBody = bodyAsString(body) match {
+      case Some(some) =>
+        logger.trace(s"req [${method.toString} $url] - $some")
+        requestWithHeaders.setBody(some)
+      case None =>
         logger.trace(s"req [${method.toString} $url]")
         requestWithHeaders
     }
@@ -82,52 +73,12 @@ object RestClient {
     }
   }
 
-  /**
-   * JSON REST API HTTP request + JSON
-   *
-   * @param request Request in format "[METHOD] URL", e.g. "GET https://api.github.com". By default method is GET.
-   * @param body Request body. Will be serialized to JSON using default json4s format.
-   * @param responsePath Json object path from the response.
-   * @param jsonFieldTransformer Json field transformer.
-   * @param executor Execution context
-   * @param formats Formats Json formats, has a default value, but could be implicitly overridden by whatever is in the context
-   * @param mf manifest
-   * @tparam A Response type
-   * @return Response
-   */
-  @deprecated("use: http() or HTTP method specific calls.", "0.7.8")
-  def request[A](request: String, body: AnyRef = None, responsePath: String = "", jsonFieldTransformer: PartialFunction[JField, JField] = defaultJsonFieldTransformer)
-                (implicit executor: ExecutionContext, mf: scala.reflect.Manifest[A], formats: Formats = DefaultFormats): Future[A] = {
-
-    val method = Method.values.map(_.toString).find(method => request.startsWith(s"$method ")).getOrElse(Method.GET.toString)
-    val url = if (request.startsWith(s"$method ")) request.substring(s"$method ".length) else request
-
-    val httpHeaders = dispatch.url(url)
-      .setMethod(method.toString)
-      .setHeader("Accept", "application/json")
-      .setHeader("Content-Type", "application/json")
-
-    val httpRequest = body match {
-      case None =>
-        logger.trace(s"req [$request]")
-        httpHeaders
-      case body: String =>
-        logger.trace(s"req [$request] - $body")
-        httpHeaders.setBody(body)
-      case anyRef =>
-        val body = write(anyRef)
-        logger.trace(s"req [$request] - $body")
-        httpHeaders.setBody(body)
-    }
-
-    Http(httpRequest OK dispatch.as.json4s.Json) map { json =>
-      logger.trace(s"rsp [$request] - ${compact(render(json))}")
-      val jsonObject = if (responsePath.isEmpty) json else json \ responsePath
-      jsonObject.transformField(jsonFieldTransformer).extract[A](DefaultFormats, mf)
-    }
-  }
-
-  def defaultJsonFieldTransformer: PartialFunction[JField, JField] = {
-    case JField(name, value) => JField(Text.toLowerCamelCase(name), value)
+  private def bodyAsString(body: Any)(implicit formats: Formats): Option[String] = body match {
+    case string: String => Some(string)
+    case Some(string: String) => Some(string)
+    case Some(some: AnyRef) => Some(write(some))
+    case any: AnyRef if any != null && any != None => Some(write(any))
+    case any if any != null && any != None => Some(any.toString)
+    case _ => None
   }
 }
