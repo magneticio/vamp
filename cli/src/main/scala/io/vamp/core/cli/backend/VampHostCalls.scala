61ed849536afca5bd65d2d2be7c2367ec97bc569
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2015-06-30 12:42:06 +0200

    bug fix: cli and gzip response

--- cli/src/main/scala/io/vamp/core/cli/backend/VampHostCalls.scala
+++ cli/src/main/scala/io/vamp/core/cli/backend/VampHostCalls.scala
@@ -1,12 +1,12 @@
 package io.vamp.core.cli.backend
 
+import io.vamp.common.http.RestClient.Method
 import io.vamp.common.http.{RestApiContentTypes, RestApiMarshaller, RestClient}
 import io.vamp.core.cli.commandline.CommandLineBasics
 import io.vamp.core.cli.serializers.Deserialization
 import io.vamp.core.model.artifact._
 import io.vamp.core.model.reader._
 import io.vamp.core.model.serialization.CoreSerializationFormat
-import org.json4s.JsonAST._
 import org.json4s.native._
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -15,31 +15,13 @@ import scala.concurrent.{Await, Future}
 import scala.language.{implicitConversions, postfixOps}
 import scala.util.{Failure, Success}
 
-object VampHostCalls extends Deserialization with RestApiMarshaller with RestApiContentTypes with CommandLineBasics {
+object VampHostCalls extends Deserialization with RestSupport with RestApiMarshaller with RestApiContentTypes with CommandLineBasics {
 
   implicit val formats = CoreSerializationFormat.default
   val timeout = 30 seconds
 
   def getDeploymentAsBlueprint(deploymentId: String)(implicit vampHost: String): Option[Blueprint] =
     sendAndWaitYaml[String](s"GET $vampHost/api/v1/deployments/$deploymentId?as_blueprint=true").map(BlueprintReader.read(_))
-
-  private def sendAndWaitYaml[A](request: String, body: Option[String] = None): Option[A] = {
-    try {
-      val futureResult: Future[A] = YamlRestClient.request(request, body = body)
-      // Block until response ready (nothing else to do anyway)
-      Await.result(futureResult, timeout)
-      futureResult.value.get match {
-        case Success(result) => Some(result)
-        case Failure(error) => terminateWithError(error.getMessage)
-          None
-      }
-    }
-    catch {
-      case e: Exception => terminateWithError(e.getMessage)
-        None
-    }
-  }
-
 
   def createBreed(breedDefinition: String, jsonOutput: Boolean = false)(implicit vampHost: String): Option[Breed] = {
     sendAndWaitYaml[String](s"POST $vampHost/api/v1/breeds", Some(breedDefinition)) match {
@@ -129,7 +111,7 @@ object VampHostCalls extends Deserialization with RestApiMarshaller with RestApi
 
 
   def getBreeds(implicit vampHost: String): List[Breed] =
-    sendAndWait[List[DefaultBreedSerialized]](s"GET $vampHost/api/v1/breeds") match {
+    sendAndWaitJson[List[DefaultBreedSerialized]](s"GET $vampHost/api/v1/breeds") match {
       case Some(breeds) => breeds.map(breedSerialized2DefaultBreed)
       case None => List.empty
     }
@@ -147,70 +129,49 @@ object VampHostCalls extends Deserialization with RestApiMarshaller with RestApi
 
 
   def getBlueprints(implicit vampHost: String): List[DefaultBlueprint] =
-    sendAndWait[List[BlueprintSerialized]](s"GET $vampHost/api/v1/blueprints") match {
+    sendAndWaitJson[List[BlueprintSerialized]](s"GET $vampHost/api/v1/blueprints") match {
       case Some(blueprints) => blueprints.map(blueprintSerialized2DefaultBlueprint)
       case None => List.empty
     }
 
   def getDeployments(implicit vampHost: String): List[Deployment] =
-    sendAndWait[List[DeploymentSerialized]](s"GET $vampHost/api/v1/deployments") match {
+    sendAndWaitJson[List[DeploymentSerialized]](s"GET $vampHost/api/v1/deployments") match {
       case Some(deployments) => deployments.map(deploymentSerialized2Deployment)
       case None => List.empty
     }
 
   def getEscalations(implicit vampHost: String): List[Escalation] =
-    sendAndWait[List[Map[String, _]]](s"GET $vampHost/api/v1/escalations") match {
+    sendAndWaitJson[List[Map[String, _]]](s"GET $vampHost/api/v1/escalations") match {
       case Some(ser) => ser.map(map2Escalation)
       case None => List.empty
     }
 
   def getFilters(implicit vampHost: String): List[DefaultFilter] =
-    sendAndWait[List[Map[String, String]]](s"GET $vampHost/api/v1/filters") match {
+    sendAndWaitJson[List[Map[String, String]]](s"GET $vampHost/api/v1/filters") match {
       case Some(ser) => ser.map(filterSerialized2DefaultFilter)
       case None => List.empty
     }
 
   def getRoutings(implicit vampHost: String): List[Routing] =
-    sendAndWait[List[RoutingSerialized]](s"GET $vampHost/api/v1/routings") match {
+    sendAndWaitJson[List[RoutingSerialized]](s"GET $vampHost/api/v1/routings") match {
       case Some(ser) => ser.map(routingSerialized2DefaultRouting)
       case None => List.empty
     }
 
   def getScales(implicit vampHost: String): List[Scale] =
-    sendAndWait[List[ScaleSerialized]](s"GET $vampHost/api/v1/scales") match {
+    sendAndWaitJson[List[ScaleSerialized]](s"GET $vampHost/api/v1/scales") match {
       case Some(ser) => ser.map(scaleSerializedToScale)
       case None => List.empty
     }
 
   def getSlas(implicit vampHost: String): List[Sla] =
-    sendAndWait[List[Map[String, _]]](s"GET $vampHost/api/v1/slas") match {
+    sendAndWaitJson[List[Map[String, _]]](s"GET $vampHost/api/v1/slas") match {
       case Some(slas) => slas.flatMap(sla => mapToSla(Some(sla)))
       case None => List.empty
     }
 
-  private def sendAndWait[A](request: String, body: AnyRef = None)(implicit m: Manifest[A]): Option[A] = {
-    try {
-      val futureResult: Future[A] = RestClient.request[A](request, body = body, jsonFieldTransformer = nonModifyingJsonFieldTransformer)
-      // Block until response ready (nothing else to do anyway)
-      Await.result(futureResult, timeout)
-      futureResult.value.get match {
-        case Success(result) => Some(result)
-        case Failure(error) => terminateWithError(error.getMessage)
-          None
-      }
-    }
-    catch {
-      case e: Exception => terminateWithError(e.getMessage)
-        None
-    }
-  }
-
-  private def nonModifyingJsonFieldTransformer: PartialFunction[JField, JField] = {
-    case JField(name, value) => JField(name, value)
-  }
-
   def createBreed(breed: DefaultBreed)(implicit vampHost: String): Option[DefaultBreed] =
-    sendAndWait[DefaultBreedSerialized](s"POST $vampHost/api/v1/breeds", breed).map(breedSerialized2DefaultBreed)
+    sendAndWaitJson[DefaultBreedSerialized](s"POST $vampHost/api/v1/breeds", breed).map(breedSerialized2DefaultBreed)
 
   def deploy(definition: String)(implicit vampHost: String): Option[Deployment] =
     sendAndWaitYaml[String](s"POST $vampHost/api/v1/deployments", body = Some(definition)).map(DeploymentReader.read(_))
@@ -245,4 +206,37 @@ object VampHostCalls extends Deserialization with RestApiMarshaller with RestApi
   def info(implicit vampHost: String) =
     sendAndWaitYaml[String](s"GET $vampHost/api/v1/info", None)
 
+}
+
+trait RestSupport {
+  this: CommandLineBasics =>
+
+  def timeout: Duration
+
+  def sendAndWaitYaml[A](request: String, body: Option[String] = None)(implicit m: Manifest[A]): Option[A] =
+    sendAndWait[A](request, body, List("Accept" -> "application/x-yaml", "Content-Type" -> "application/x-yaml", RestClient.acceptEncodingIdentity))
+
+  def sendAndWaitJson[A](request: String, body: AnyRef = None)(implicit m: Manifest[A]): Option[A] =
+    sendAndWait[A](request, body, RestClient.jsonHeaders :+ RestClient.acceptEncodingIdentity)
+
+  private def sendAndWait[A](request: String, body: AnyRef, headers: List[(String, String)])(implicit m: Manifest[A]): Option[A] = {
+    try {
+      val upper = request.toUpperCase
+      val method = Method.values.find(method => upper.startsWith(s"${method.toString} ")).getOrElse(Method.GET)
+      val url = if (upper.startsWith(s"${method.toString} ")) request.substring(s"${method.toString} ".length) else request
+
+      val futureResult: Future[A] = RestClient.http[A](method, url, body, headers)
+      // Block until response ready (nothing else to do anyway)
+      Await.result(futureResult, timeout)
+      futureResult.value.get match {
+        case Success(result) => Some(result)
+        case Failure(error) => terminateWithError(error.getMessage)
+          None
+      }
+    }
+    catch {
+      case e: Exception => terminateWithError(e.getMessage)
+        None
+    }
+  }
 }
