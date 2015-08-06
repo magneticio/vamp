7916743804b4d1e17316cc0a3c5121f413c63f10
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2015-08-06 10:33:07 +0200

    bug fix: sse as in specs

--- src/main/scala/io/vamp/common/http/SseDirectives.scala
+++ src/main/scala/io/vamp/common/http/SseDirectives.scala
@@ -2,6 +2,7 @@ package io.vamp.common.http
 
 import akka.actor._
 import spray.can.Http
+import spray.http.HttpHeaders.{RawHeader, `Cache-Control`}
 import spray.http.MediaTypes._
 import spray.http._
 import spray.routing.Directives._
@@ -15,13 +16,22 @@ import scala.language.{implicitConversions, postfixOps}
  */
 trait SseDirectives {
 
+  case class SseMessage(event: Option[String], data: String)
+
   case class RegisterClosedHandler(handler: () => Unit)
 
   object CloseConnection
 
   val `text/event-stream` = register(MediaType.custom(mainType = "text", subType = "event-stream", compressible = true, binary = true))
 
-  def responseStart = HttpResponse(headers = Nil, entity = "\n")
+  def responseStart = HttpResponse(
+    headers = `Cache-Control`(CacheDirectives.`no-cache`) ::
+      RawHeader("Access-Control-Allow-Methods", "GET, POST") ::
+      RawHeader("Access-Control-Allow-Headers", "Cache-Control") ::
+      RawHeader("Access-Control-Max-Age", "86400") ::
+      Nil,
+    entity = ":\n"
+  )
 
   def sseKeepAliveTimeout = 15 seconds
 
@@ -39,16 +49,19 @@ trait SseDirectives {
             context.setReceiveTimeout(sseKeepAliveTimeout)
 
             def receive = {
-              case data: String =>
-                ctx.responder ! MessageChunk(s"$data\n")
+              case SseMessage(event, data) =>
+                val eventString = event.map(ev => s"event: $ev\n").getOrElse("")
+                val dataString = data.split("\n").map(d => s"data: $d\n").mkString
+                ctx.responder ! MessageChunk(s"$eventString$dataString\n")
               case CloseConnection =>
                 ctx.responder ! ChunkedMessageEnd
               case ReceiveTimeout =>
-                ctx.responder ! MessageChunk("\n") // keep connection alive
+                ctx.responder ! MessageChunk(":\n") // keep connection alive
               case RegisterClosedHandler(handler) => closedHandlers ::= handler
               case _: Http.ConnectionClosed =>
                 closedHandlers.foreach(_())
                 context.stop(self)
+              case _ =>
             }
           }
         }
