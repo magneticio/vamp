67ec311618eac32c467d2ce87d79e27d4a0791d5
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2015-08-21 17:51:19 +0200

    bug fix: pagination support

--- persistence/src/main/scala/io/vamp/core/persistence/PaginationSupport.scala
+++ persistence/src/main/scala/io/vamp/core/persistence/PaginationSupport.scala
@@ -1,13 +1,13 @@
 package io.vamp.core.persistence
 
+import akka.pattern.ask
+import akka.util.Timeout
 import io.vamp.common.akka.{ActorSupport, ExecutionContextProvider}
 import io.vamp.common.http.OffsetResponseEnvelope
 import io.vamp.common.notification.NotificationProvider
 import io.vamp.core.model.artifact.Artifact
-import io.vamp.core.model.event.{EventQuery, Event}
+import io.vamp.core.model.event.{Event, EventQuery}
 import io.vamp.core.persistence.notification.PersistenceOperationFailure
-import akka.pattern.ask
-import akka.util.Timeout
 import io.vamp.core.pulse.{EventRequestEnvelope, PulseActor}
 
 import scala.concurrent.Future
@@ -17,19 +17,16 @@ import scala.util.{Failure, Success}
 trait PaginationSupport {
   this: ExecutionContextProvider =>
 
-  def allPages[T](onePage: (Int, Int) => Future[_ <: OffsetResponseEnvelope[T]])(implicit timeout: Timeout): Future[List[T]] = {
-    val perPage = ArtifactResponseEnvelope.maxPerPage
-    onePage(0, perPage) flatMap {
-      case envelope: OffsetResponseEnvelope[T] =>
-        val (total, pageList) = envelope.total -> envelope.response
-        if (total > pageList.size) {
-          val futures = Future(pageList) :: (1 until (total / perPage + (if (total % perPage == 0) 0 else 1)).toInt).map({ case i =>
-            onePage(i * perPage, perPage).map(_.response)
-          }).toList
-          Future.sequence(futures.map(_.map(Success(_)).recover({ case x => Failure(x) }))).map(_.filter(_.isSuccess).flatMap(_.get))
-        }
-        else Future(pageList)
-    }
+  def allPages[T](onePage: (Int, Int) => Future[_ <: OffsetResponseEnvelope[T]], perPage: Int = 10): Future[List[T]] = onePage(1, perPage) flatMap {
+    case envelope: OffsetResponseEnvelope[T] =>
+      val (total, pageList) = envelope.total -> envelope.response
+      if (total > pageList.size) {
+        val futures = Future(pageList) :: (2 to (total / perPage + (if (total % perPage == 0) 0 else 1)).toInt).map({ case i =>
+          onePage(i, perPage).map(_.response)
+        }).toList
+        Future.sequence(futures.map(_.map(Success(_)).recover({ case x => Failure(x) }))).map(_.filter(_.isSuccess).flatMap(_.get))
+      }
+      else Future(pageList)
   }
 }
 
@@ -41,7 +38,7 @@ trait ArtifactPaginationSupport extends PaginationSupport {
       case envelope: OffsetResponseEnvelope[_] => envelope.asInstanceOf[OffsetResponseEnvelope[T]]
       case other => throwException(PersistenceOperationFailure(other))
     }
-  })
+  }, ArtifactResponseEnvelope.maxPerPage)
 }
 
 trait EventPaginationSupport extends PaginationSupport {
@@ -52,5 +49,5 @@ trait EventPaginationSupport extends PaginationSupport {
       case envelope: OffsetResponseEnvelope[_] => envelope.asInstanceOf[OffsetResponseEnvelope[Event]]
       case other => throwException(PersistenceOperationFailure(other))
     }
-  })
+  }, EventRequestEnvelope.maxPerPage)
 }
