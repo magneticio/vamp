8ccdd719906f62bb4e6549eb8dc245e845c4d07d
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2015-08-25 16:14:49 +0200

    removed actor selection from IoC

--- src/main/scala/io/vamp/common/akka/IoC.scala
+++ src/main/scala/io/vamp/common/akka/IoC.scala
@@ -8,16 +8,28 @@ object IoC {
 
   private val aliases: mutable.Map[String, ActorDescription] = mutable.Map()
 
+  private val actorRefs: mutable.Map[String, ActorRef] = mutable.Map()
+
   def alias(from: ActorDescription) = aliases.getOrElse(from.name, from)
 
   def alias(from: ActorDescription, to: ActorDescription) = aliases.put(from.name, to)
 
-  def createActor(actorDescription: ActorDescription, args: Any*)(implicit mailbox: String = "akka.actor.default-mailbox", actorSystem: ActorSystem) = {
+  def createActor(actorDescription: ActorDescription, args: Any*)(implicit mailbox: String = "akka.actor.default-mailbox", actorSystem: ActorSystem): ActorRef = {
+
     val description = alias(actorDescription)
-    actorSystem.actorOf(description.props(args: _*).withMailbox(mailbox), description.name)
+    val actorRef = actorSystem.actorOf(description.props(args: _*).withMailbox(mailbox), description.name)
+
+    actorRefs.put(description.name, actorRef)
+    actorRef
   }
 
-  def actorFor(actorDescription: ActorDescription)(implicit actorSystem: ActorSystem) = {
-    actorSystem.actorSelection(s"/user/${alias(actorDescription).name}")
+  def actorFor(actorDescription: ActorDescription)(implicit actorSystem: ActorSystem): ActorRef = {
+
+    val description = alias(actorDescription)
+
+    actorRefs.get(description.name) match {
+      case Some(actorRef) => actorRef
+      case _ => throw new RuntimeException(s"No actor reference for: ${actorDescription.name}")
+    }
   }
 }
