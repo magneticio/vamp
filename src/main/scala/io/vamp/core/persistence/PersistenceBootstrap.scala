82d22a352f2ef7c3f129a5b44ef763d7c7f53ad7
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2015-08-26 15:34:26 +0200

    common update

--- src/main/scala/io/vamp/core/persistence/PersistenceBootstrap.scala
+++ src/main/scala/io/vamp/core/persistence/PersistenceBootstrap.scala
@@ -1,6 +1,6 @@
 package io.vamp.core.persistence
 
-import akka.actor.{ ActorSystem, Props }
+import akka.actor.ActorSystem
 import com.typesafe.config.ConfigFactory
 import io.vamp.common.akka.Bootstrap.{ Shutdown, Start }
 import io.vamp.common.akka.{ Bootstrap, IoC }
@@ -12,16 +12,16 @@ object PersistenceBootstrap extends Bootstrap {
   def run(implicit actorSystem: ActorSystem) = storageType match {
     case "in-memory" ⇒
       IoC.alias[PersistenceActor, InMemoryPersistenceActor]
-      IoC.createActor(Props(classOf[InMemoryPersistenceActor])) ! Start
+      IoC.createActor[InMemoryPersistenceActor] ! Start
 
     case "elasticsearch" ⇒
       IoC.alias[PersistenceActor, ElasticsearchPersistenceActor]
-      IoC.createActor(Props(classOf[ElasticsearchPersistenceInitializationActor])) ! Start
-      IoC.createActor(Props(classOf[ElasticsearchPersistenceActor])) ! Start
+      IoC.createActor[ElasticsearchPersistenceInitializationActor] ! Start
+      IoC.createActor[ElasticsearchPersistenceActor] ! Start
 
     case _ ⇒
       IoC.alias[PersistenceActor, JdbcPersistenceActor]
-      IoC.createActor(Props(classOf[JdbcPersistenceActor])) ! Start
+      IoC.createActor[JdbcPersistenceActor] ! Start
   }
 
   override def shutdown(implicit actorSystem: ActorSystem): Unit = {
