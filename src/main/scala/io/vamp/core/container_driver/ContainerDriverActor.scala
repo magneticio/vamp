790befd547a59c1aab6ebe5802da3548b22339bd
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2015-08-28 15:45:06 +0200

    event failure notifications

--- src/main/scala/io/vamp/core/container_driver/ContainerDriverActor.scala
+++ src/main/scala/io/vamp/core/container_driver/ContainerDriverActor.scala
@@ -3,9 +3,11 @@ package io.vamp.core.container_driver
 import akka.util.Timeout
 import com.typesafe.config.ConfigFactory
 import io.vamp.common.akka._
+import io.vamp.common.notification.Notification
 import io.vamp.common.vitals.InfoRequest
 import io.vamp.core.container_driver.notification.{ ContainerDriverNotificationProvider, ContainerResponseError, UnsupportedContainerDriverRequest }
 import io.vamp.core.model.artifact.{ Deployment, DeploymentCluster, DeploymentService }
+import io.vamp.core.pulse.notification.PulseFailureNotifier
 
 import scala.concurrent.duration._
 
@@ -23,7 +25,7 @@ object ContainerDriverActor {
 
 }
 
-class ContainerDriverActor(driver: ContainerDriver) extends CommonSupportForActors with ContainerDriverNotificationProvider {
+class ContainerDriverActor(driver: ContainerDriver) extends PulseFailureNotifier with CommonSupportForActors with ContainerDriverNotificationProvider {
 
   import io.vamp.core.container_driver.ContainerDriverActor._
 
@@ -32,19 +34,13 @@ class ContainerDriverActor(driver: ContainerDriver) extends CommonSupportForActo
   override def errorNotificationClass = classOf[ContainerResponseError]
 
   def receive = {
-    case InfoRequest ⇒ reply {
-      driver.info
-    }
-    case All ⇒ reply {
-      driver.all
-    }
-    case Deploy(deployment, cluster, service, update) ⇒ reply {
-      driver.deploy(deployment, cluster, service, update)
-    }
-    case Undeploy(deployment, service) ⇒ reply {
-      driver.undeploy(deployment, service)
-    }
+    case InfoRequest ⇒ reply(driver.info)
+    case All ⇒ reply(driver.all)
+    case Deploy(deployment, cluster, service, update) ⇒ reply(driver.deploy(deployment, cluster, service, update))
+    case Undeploy(deployment, service) ⇒ reply(driver.undeploy(deployment, service))
     case any ⇒ unsupported(UnsupportedContainerDriverRequest(any))
   }
+
+  override def failure(failure: Any, `class`: Class[_ <: Notification] = errorNotificationClass) = super[PulseFailureNotifier].failure(failure, `class`)
 }
 
