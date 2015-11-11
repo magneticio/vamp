340ce93e26dadc674bcc3dde9965b3c0668a4a47
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2015-11-11 20:09:29 +0100

    kibana logstash indx initialization

--- src/main/scala/io/vamp/pulse/elasticsearch/PulseInitializationActor.scala
+++ src/main/scala/io/vamp/pulse/elasticsearch/PulseInitializationActor.scala
@@ -1,18 +1,17 @@
 package io.vamp.pulse.elasticsearch
 
 import io.vamp.pulse.PulseActor
+import io.vamp.pulse.elasticsearch.ElasticsearchInitializationActor.TemplateDefinition
 import io.vamp.pulse.notification.PulseNotificationProvider
 
 import scala.io.Source
 
 class PulseInitializationActor extends ElasticsearchInitializationActor with PulseNotificationProvider {
 
-  lazy val timeout = PulseActor.timeout
-
   lazy val elasticsearchUrl = PulseActor.elasticsearchUrl
 
-  lazy val templates = {
+  override lazy val templates = {
     def load(name: String) = Source.fromInputStream(getClass.getResourceAsStream(s"$name.json")).mkString.replace("$NAME", PulseActor.indexName)
-    List("template", "template-event").map(template ⇒ s"${PulseActor.indexName}-$template" -> load(template)).toMap
+    List("template", "template-event").map(template ⇒ TemplateDefinition(s"${PulseActor.indexName}-$template", load(template)))
   }
 }
