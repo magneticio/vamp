693c8e955ad25a24864573332e011effdcc5b247
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2015-12-23 21:00:22 +0100

    adding back host conf

--- dictionary/src/main/scala/io/vamp/dictionary/DictionaryActor.scala
+++ dictionary/src/main/scala/io/vamp/dictionary/DictionaryActor.scala
@@ -29,6 +29,7 @@ class DictionaryActor extends CommonSupportForActors with DictionaryNotification
 
   implicit val timeout = DictionaryActor.timeout
 
+  private val hostResolver = toRegExp(DictionaryActor.hostResolver)
   private val containerScale = toRegExp(DictionaryActor.containerScale)
 
   private def toRegExp(string: String) = {
@@ -46,6 +47,9 @@ class DictionaryActor extends CommonSupportForActors with DictionaryNotification
   }
 
   private def get(key: String) = key match {
+
+    case hostResolver(_*) ⇒
+      ConfigFactory.load().getString("vamp.gateway-driver.host")
 
     case containerScale(deployment, cluster, service) ⇒
       val config = ConfigFactory.load()
