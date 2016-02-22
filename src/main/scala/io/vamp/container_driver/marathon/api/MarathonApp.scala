eb73a6f76722978fd761e0bef4e1c570d271da11
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2016-02-22 22:24:59 +0100

    deployment service arguments

--- src/main/scala/io/vamp/container_driver/marathon/api/MarathonApp.scala
+++ src/main/scala/io/vamp/container_driver/marathon/api/MarathonApp.scala
@@ -6,6 +6,6 @@ case class MarathonApp(id: String, container: Option[Container], instances: Int,
 
 case class Container(docker: Docker, `type`: String = "DOCKER")
 
-case class Docker(image: String, portMappings: List[DockerPortMapping], parameters: List[DockerParameter] = List(DockerParameter("security-opt", "seccomp:unconfined")), network: String = "BRIDGE")
+case class Docker(image: String, portMappings: List[DockerPortMapping], parameters: List[DockerParameter], network: String = "BRIDGE")
 
 case class DockerParameter(key: String, value: String)
