b0f57a029e4498d7414f38c699c59d35a9a1bc25
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2016-02-24 00:37:17 +0100

    cleaning up old docker driver implementation

--- container_driver/src/main/scala/io/vamp/container_driver/docker/wrapper/model/Image.scala
+++
@@ -1,3 +0,0 @@
-package io.vamp.container_driver.docker.wrapper.model
-
-case class Image(id: String, repoTags: List[String] = Nil)
--- container_driver/src/main/scala/io/vamp/container_driver/marathon/api/MarathonApp.scala
+++ container_driver/src/main/scala/io/vamp/container_driver/marathon/api/MarathonApp.scala
@@ -1,11 +1,11 @@
 package io.vamp.container_driver.marathon.api
 
-import io.vamp.container_driver.docker.DockerPortMapping
+import io.vamp.container_driver.ContainerPortMapping
 
 case class MarathonApp(id: String, container: Option[Container], instances: Int, cpus: Double, mem: Double, env: Map[String, String], cmd: Option[String])
 
 case class Container(docker: Docker, `type`: String = "DOCKER")
 
-case class Docker(image: String, portMappings: List[DockerPortMapping], parameters: List[DockerParameter], network: String = "BRIDGE")
+case class Docker(image: String, portMappings: List[ContainerPortMapping], parameters: List[DockerParameter], network: String = "BRIDGE")
 
 case class DockerParameter(key: String, value: String)
