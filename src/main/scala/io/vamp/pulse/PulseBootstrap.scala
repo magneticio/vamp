5ff1e1c7e36788e191e207bf8c400fa658108156
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2016-03-20 22:41:37 +0100

    moving init to lifter module, closes #394

--- src/main/scala/io/vamp/pulse/PulseBootstrap.scala
+++ src/main/scala/io/vamp/pulse/PulseBootstrap.scala
@@ -2,13 +2,11 @@ package io.vamp.pulse
 
 import akka.actor.{ ActorRef, ActorSystem }
 import io.vamp.common.akka.{ Bootstrap, IoC }
-import io.vamp.pulse.elasticsearch.PulseInitializationActor
 import io.vamp.pulse.notification.PulseNotificationProvider
 
 object PulseBootstrap extends Bootstrap with PulseNotificationProvider {
 
   def createActors(implicit actorSystem: ActorSystem): List[ActorRef] = List(
-    IoC.createActor[PulseInitializationActor],
     IoC.createActor[PulseActor]
   )
 }
--- src/main/scala/io/vamp/pulse/notification/PulseNotification.scala
+++ src/main/scala/io/vamp/pulse/notification/PulseNotification.scala
@@ -7,8 +7,6 @@ case class UnsupportedPulseRequest(request: Any) extends Notification with Reque
 
 case class PulseResponseError(reason: Any) extends Notification with ErrorNotification
 
-object ElasticsearchInitializationTimeoutError extends Notification
-
 case class EventIndexError(reason: Any) extends Notification with ErrorNotification
 
 case class EventQueryError(reason: Any) extends Notification with ErrorNotification
