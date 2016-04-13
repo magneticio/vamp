9f4e4f95db08b8827cea70bae5a6666483c21ca2
Emiliano <emiliano.martinez.contractor@bbva.com>
2016-04-13 13:17:13 +0200

    Some fixes to support new ContainerDrivers specification using actors

---
+++ container_driver/src/main/scala/io/vamp/container_driver/rancher/RancherDriverActor.scala
@@ -0,0 +1,17 @@
+package io.vamp.container_driver.rancher
+
+import io.vamp.common.vitals.InfoRequest
+import io.vamp.container_driver.ContainerDriverActor.{ All, Deploy, Undeploy }
+import io.vamp.container_driver._
+import io.vamp.container_driver.notification.UnsupportedContainerDriverRequest
+
+class DockerDriverActor extends RancherDriver with ContainerDriverActor {
+
+  def receive = {
+    case InfoRequest ⇒ reply(info)
+    case All         ⇒ reply(all)
+    case d: Deploy   ⇒ reply(deploy(d.deployment, d.cluster, d.service, d.update))
+    case u: Undeploy ⇒ reply(undeploy(u.deployment, u.service))
+    case any         ⇒ unsupported(UnsupportedContainerDriverRequest(any))
+  }
+}
