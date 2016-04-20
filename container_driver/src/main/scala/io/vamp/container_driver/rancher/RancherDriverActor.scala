cf0235b74e63e5761aed1e0aafb9cb5ce9c0003f
Emiliano <emiliano.martinez.contractor@bbva.com>
2016-04-20 22:05:05 +0200

    add Rancher Basic Authentication to include API credentials

--- container_driver/src/main/scala/io/vamp/container_driver/rancher/RancherDriverActor.scala
+++ container_driver/src/main/scala/io/vamp/container_driver/rancher/RancherDriverActor.scala
@@ -4,6 +4,16 @@ import io.vamp.common.vitals.InfoRequest
 import io.vamp.container_driver.ContainerDriverActor.{ All, Deploy, Undeploy }
 import io.vamp.container_driver._
 import io.vamp.container_driver.notification.UnsupportedContainerDriverRequest
+import org.apache.commons.codec.Charsets
+
+private[rancher] object Credentials {
+
+  def credentials(user: String, password: String): String = {
+    val encoder = new sun.misc.BASE64Encoder
+    val base64Auth = s"${user}:${password}"
+    encoder.encode(base64Auth.getBytes(Charsets.UTF_8)).replace("\n", "")
+  }
+}
 
 class RancherDriverActor extends RancherDriver with ContainerDriverActor {
 
