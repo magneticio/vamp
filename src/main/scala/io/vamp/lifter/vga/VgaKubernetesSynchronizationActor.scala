d97791f9e0bff04b79c400fdaac772876a13938a
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2016-05-29 12:12:03 +0200

    kuberntes lifter daemon set service type

--- src/main/scala/io/vamp/lifter/vga/VgaKubernetesSynchronizationActor.scala
+++ src/main/scala/io/vamp/lifter/vga/VgaKubernetesSynchronizationActor.scala
@@ -1,12 +1,13 @@
 package io.vamp.lifter.vga
 
 import io.vamp.common.akka._
-import io.vamp.container_driver.kubernetes.DaemonSet
+import io.vamp.container_driver.kubernetes.{ DaemonSet, KubernetesServiceType }
 import io.vamp.container_driver.{ ContainerDriverActor, Docker }
 import io.vamp.lifter.notification.LifterNotificationProvider
 import io.vamp.lifter.vga.VgaKubernetesSynchronizationActor.Synchronize
 
 import scala.language.postfixOps
+import scala.util.Try
 
 class VgaKubernetesSynchronizationSchedulerActor extends SchedulerActor with LifterNotificationProvider {
 
@@ -25,6 +26,8 @@ class VgaKubernetesSynchronizationActor extends VgaSynchronizationActor {
 
   import VgaKubernetesSynchronizationActor._
 
+  val kubernetesServiceType = Try(KubernetesServiceType.withName(configuration.getString("kubernetes.service-type"))).map(Option(_)).getOrElse(None)
+
   def receive = {
     case Synchronize ⇒ synchronize()
     case _           ⇒
@@ -42,6 +45,7 @@ class VgaKubernetesSynchronizationActor extends VgaSynchronizationActor {
       ),
       cpu = cpu,
       mem = mem,
+      serviceType = kubernetesServiceType,
       args = arguments)
   }
 }
