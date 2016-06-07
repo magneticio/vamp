cc52e537334666b1720bee94da573de5406ad259
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2016-06-07 16:16:26 +0200

    creating artifact by lifter, closes #535

---
+++ src/main/resources/io/vamp/lifter/artifact/workflows/metrics.js
@@ -0,0 +1,102 @@
+var _ = require('lodash');
+
+var elasticsearch = require('elasticsearch');
+
+var esClient = new elasticsearch.Client({
+  host: process.env.VAMP_ELASTICSEARCH_CONNECTION,
+  log: 'info'
+});
+
+var index = function(tags, value) {
+  esClient.index({
+      index: 'vamp-pulse',
+      type: 'event',
+      body: {
+        tags: tags,
+        value: value
+      }
+    },
+    function (error, response) {}
+  );
+}
+
+var seconds = 30;
+
+var metrics = function(gateways) {
+  _.forEach(gateways, function(gateway) {
+
+    esClient.search({
+      index: 'logstash-*',
+      type: 'haproxy',
+      body: {
+        query: {
+          filtered: {
+            query: {
+              match_all: {}
+            },
+            filter: {
+              bool: {
+                must: [
+                  {
+                    term: {
+                      ft: gateway.lookup_name
+                    }
+                  },
+                  {
+                    range: {
+                      "@timestamp": {
+                        gt: "now-" + seconds + "s"
+                      }
+                    }
+                  }
+                ]
+              }
+            }
+          }
+        },
+        aggregations: {
+          Tt: {
+            avg: {
+              field: "Tt"
+            }
+          }
+        },
+        size: 0
+      }
+    }).then(function (resp) {
+
+        var total = resp.hits.total;
+        var rate = Math.round(total / seconds * 100) / 100;
+        var responseTime = Math.round(resp.aggregations.Tt.value * 100) / 100;
+
+        console.log("gateway       : " + gateway.name);
+        console.log("total         : " + total);
+        console.log("rate          : " + rate);
+        console.log("response time : " + responseTime);
+
+        index(["gateways", "gateways:" + gateway.lookup_name, "metrics", "metrics:rate"], rate);
+        index(["gateways", "gateways:" + gateway.lookup_name, "metrics", "metrics:response-time"], responseTime);
+
+    }, function (err) {
+        console.log(err.message);
+    });
+  });
+};
+
+var zookeeper = require('node-zookeeper-client');
+
+var zkClient = zookeeper.createClient(process.env.VAMP_KEY_VALUE_STORE_CONNECTION);
+var path = '/vamp/gateways';
+
+zkClient.once('connected', function () {
+  zkClient.getData(
+    path,
+    function () {},
+    function (err, data, stat) {
+      if (err) throw err;
+      if (data) metrics(JSON.parse(data.toString()));
+      zkClient.close();
+    });
+});
+
+zkClient.connect();
--- src/main/scala/io/vamp/lifter/artifact/ArtifactInitializationActor.scala
+++ src/main/scala/io/vamp/lifter/artifact/ArtifactInitializationActor.scala
@@ -1,15 +1,37 @@
 package io.vamp.lifter.artifact
 
+import java.nio.file.Paths
+
+import akka.pattern.ask
+import com.typesafe.config.ConfigFactory
 import io.vamp.common.akka.CommonSupportForActors
+import io.vamp.common.akka.IoC._
 import io.vamp.lifter.notification.LifterNotificationProvider
+import io.vamp.model.workflow.DefaultWorkflow
+import io.vamp.operation.controller.ArtifactApiController
+import io.vamp.persistence.db.PersistenceActor
+
+import scala.collection.JavaConverters._
+import scala.concurrent.Future
+import scala.io.Source
 
 object ArtifactInitializationActor {
+
   private[artifact] object Load
+
 }
 
-class ArtifactInitializationActor extends CommonSupportForActors with LifterNotificationProvider {
+class ArtifactInitializationActor extends ArtifactApiController with CommonSupportForActors with LifterNotificationProvider {
 
   import ArtifactInitializationActor._
+
+  private implicit val timeout = PersistenceActor.timeout
+
+  private val config = ConfigFactory.load().getConfig("vamp.lifter.artifact")
+
+  private val force = config.getBoolean("override")
+
+  private val resources = config.getStringList("resources").asScala
 
   def receive = {
     case Load ⇒ load()
@@ -18,8 +40,38 @@ class ArtifactInitializationActor extends CommonSupportForActors with LifterNoti
 
   override def preStart(): Unit = self ! Load
 
-  private def load() = {
+  private def load() = resources.map(Paths.get(_)).foreach { path ⇒
 
+    val `type` = path.getParent.toString
+    val fileName = path.getFileName.toString
+    val name = fileName.substring(0, fileName.lastIndexOf("."))
+    val source = Source.fromInputStream(getClass.getResourceAsStream(path.toString)).mkString
+
+    exists(`type`, name).map {
+      case true ⇒
+        if (force) {
+          log.info(s"Updating artifact: ${`type`}/$name")
+          create(`type`, fileName, name, source)
+        } else
+          log.info(s"Ignoring creation of artifact because it exists: ${`type`}/$name")
+
+      case false ⇒
+        log.info(s"Creating artifact: ${`type`}/$name")
+        create(`type`, fileName, name, source)
+    }
+  }
+
+  private def exists(`type`: String, name: String): Future[Boolean] = {
+    readArtifact(`type`, name, expandReferences = false, onlyReferences = false).map {
+      case Some(_) ⇒ true
+      case _       ⇒ false
+    }
+  }
+
+  private def create(`type`: String, fileName: String, name: String, source: String) = {
+    if (`type` == "workflows" && fileName.endsWith(".js"))
+      actorFor[PersistenceActor] ? PersistenceActor.Update(DefaultWorkflow(name, None, Option(source), None), Some(source))
+    else
+      updateArtifact(`type`, name, source, validateOnly = false)
   }
 }
-
