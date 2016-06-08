9b1674dcfa93ec1ebce05b73c8a7fd8cadb3a175
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2016-06-08 12:13:44 +0200

    retrieving aggregated values via metric endpoints, closes #643

--- operation/src/main/scala/io/vamp/operation/controller/MetricsController.scala
+++ operation/src/main/scala/io/vamp/operation/controller/MetricsController.scala
@@ -1,7 +1,15 @@
 package io.vamp.operation.controller
 
+import java.time.OffsetDateTime
+import java.time.temporal.ChronoUnit
+
+import com.typesafe.config.ConfigFactory
+import io.vamp.common.akka.IoC._
 import io.vamp.common.akka.{ ActorSystemProvider, ExecutionContextProvider, ReplyActor }
 import io.vamp.common.notification.NotificationProvider
+import io.vamp.model.event._
+import io.vamp.pulse.{ EventRequestEnvelope, EventResponseEnvelope, PulseActor }
+import akka.pattern.ask
 
 import scala.concurrent.Future
 import scala.language.postfixOps
@@ -9,8 +17,25 @@ import scala.language.postfixOps
 trait MetricsController extends GatewayDeploymentResolver {
   this: ReplyActor with ExecutionContextProvider with ActorSystemProvider with NotificationProvider ⇒
 
-  def gatewayMetrics(gateway: String, metrics: String): Future[Option[Double]] = gatewayFor(gateway) map {
-    _.map(_ ⇒ 0D)
+  private val window = ConfigFactory.load().getInt("vamp.operation.metrics.window")
+
+  def gatewayMetrics(gateway: String, metrics: String): Future[Option[Double]] = gatewayFor(gateway).flatMap {
+
+    case Some(g) ⇒
+
+      val now = OffsetDateTime.now()
+      val from = now.minus(window, ChronoUnit.SECONDS)
+
+      val tags = s"gateways:${g.lookupName}" :: s"metrics:$metrics" :: Nil
+
+      val eventQuery = EventQuery(tags.toSet, Some(TimeRange(Some(from), Some(now), includeLower = true, includeUpper = true)), None)
+
+      actorFor[PulseActor] ? PulseActor.Query(EventRequestEnvelope(eventQuery, 1, 1)) map {
+        case EventResponseEnvelope(Event(_, value, _, _) :: tail, _, _, _) ⇒ Option(value.toString.toDouble)
+        case other ⇒ None
+      }
+
+    case None ⇒ Future.successful(None)
   }
 
   def routeMetrics(gateway: String, route: String, metrics: String) = gatewayFor(gateway) map {
