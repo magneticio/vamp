c3fc316b0eea10c57b48273b0468a3bf11676614
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2016-06-30 15:50:19 +0200

    using kamon for system metrics, closes #381

--- src/main/scala/io/vamp/pulse/PulseBootstrap.scala
+++ src/main/scala/io/vamp/pulse/PulseBootstrap.scala
@@ -1,10 +1,10 @@
 package io.vamp.pulse
 
 import akka.actor.{ ActorRef, ActorSystem }
-import io.vamp.common.akka.{ Bootstrap, IoC }
+import io.vamp.common.akka.{ ActorBootstrap, IoC }
 import io.vamp.pulse.notification.PulseNotificationProvider
 
-object PulseBootstrap extends Bootstrap with PulseNotificationProvider {
+object PulseBootstrap extends ActorBootstrap with PulseNotificationProvider {
 
   def createActors(implicit actorSystem: ActorSystem): List[ActorRef] = List(
     IoC.createActor[PulseActor]
