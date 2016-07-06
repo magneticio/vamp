32d66c508fd83a784f7ca3da15dc146f24c9cd5f
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2016-07-06 12:31:04 +0200

    using git describe as version fallback

--- model/src/main/scala/io/vamp/model/Model.scala
+++ model/src/main/scala/io/vamp/model/Model.scala
@@ -4,11 +4,16 @@ import java.time.format.DateTimeFormatter._
 import java.time.{ ZoneOffset, ZonedDateTime }
 import java.util.UUID
 
+import scala.sys.process._
+import scala.util.Try
+
 object Model {
 
   val uuid = UUID.randomUUID.toString
 
-  val version = Option(getClass.getPackage.getImplementationVersion)
+  val version: Option[String] = Option(getClass.getPackage.getImplementationVersion).orElse {
+    Try(Option("git describe --tags".!!.stripLineEnd)).getOrElse(None)
+  }
 
   val runningSince = ZonedDateTime.now(ZoneOffset.UTC).format(ISO_OFFSET_DATE_TIME)
 }
