ce335cca4295f7d5de4f39318c52e46cb9c1693a
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2016-07-07 11:19:23 +0200

    kubernetes: using daemonset cmd instead of args

--- lifter/src/main/scala/io/vamp/lifter/vga/VgaKubernetesSynchronizationActor.scala
+++ lifter/src/main/scala/io/vamp/lifter/vga/VgaKubernetesSynchronizationActor.scala
@@ -45,7 +45,7 @@ class VgaKubernetesSynchronizationActor extends VgaSynchronizationActor {
       cpu = cpu,
       mem = mem,
       serviceType = kubernetesServiceType,
-      args = command
+      cmd = command
     )
   }
 }
