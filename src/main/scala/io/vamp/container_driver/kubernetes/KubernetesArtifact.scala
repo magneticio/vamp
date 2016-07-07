04b128b0d4b4cdd0fde4001cef344a2dd80a6623
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2016-07-07 13:34:04 +0200

    fixing kubernetes labels

--- src/main/scala/io/vamp/container_driver/kubernetes/KubernetesArtifact.scala
+++ src/main/scala/io/vamp/container_driver/kubernetes/KubernetesArtifact.scala
@@ -5,7 +5,16 @@ import java.net.URLEncoder
 trait KubernetesArtifact {
 
   protected def labels2json(labels: Map[String, String]) = {
-    val l = labels.map { case (k, v) ⇒ s""""$k": "$v"""" } mkString ", "
+    val l = labels.filter {
+      case (k, _) ⇒ k.matches("(([A-Za-z0-9][-A-Za-z0-9_.]*)?[A-Za-z0-9])?") && k.length < 64
+    } filter {
+      case (_, v) ⇒ v.isEmpty || (v.matches("^[a-zA-Z0-9].*[a-zA-Z0-9]$") && v.length < 64)
+    } map {
+      case (k, v) ⇒ k -> v.replaceAll("[^a-zA-Z0-9\\._-]", "_")
+    } map {
+      case (k, v) ⇒ s""""$k": "$v""""
+    } mkString ", "
+
     s""""labels": {$l}"""
   }
 
