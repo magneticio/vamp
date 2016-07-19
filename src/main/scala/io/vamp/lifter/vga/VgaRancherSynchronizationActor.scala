6bc6169058f010b80839fa8100f11efe33083376
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2016-07-19 20:36:49 +0200

    refactoring workflows - marathon implementation, closes #716

--- src/main/scala/io/vamp/lifter/vga/VgaRancherSynchronizationActor.scala
+++ src/main/scala/io/vamp/lifter/vga/VgaRancherSynchronizationActor.scala
@@ -1,9 +1,6 @@
 package io.vamp.lifter.vga
 
-import akka.pattern.ask
 import io.vamp.common.akka._
-import io.vamp.container_driver.DockerAppDriver.{ DeployDockerApp, RetrieveDockerApp }
-import io.vamp.container_driver.{ ContainerDriverActor, Docker, DockerApp }
 import io.vamp.lifter.notification.LifterNotificationProvider
 import io.vamp.lifter.vga.VgaRancherSynchronizationActor.Synchronize
 
@@ -25,37 +22,37 @@ class VgaRancherSynchronizationActor extends VgaSynchronizationActor {
   import VgaRancherSynchronizationActor._
 
   def receive = {
-    case Synchronize ⇒ synchronize()
+    case Synchronize ⇒ //synchronize()
     case _           ⇒
   }
 
-  private def synchronize() = {
-    val actor = IoC.actorFor[ContainerDriverActor]
-    actor ? RetrieveDockerApp(id) map {
-      case Some(_) ⇒
-      case None    ⇒ actor ! DeployDockerApp(request, update = false)
-      case _       ⇒
-    }
-  }
-
-  private def request = {
-    DockerApp(
-      id = id,
-      container = Option(
-        Docker(
-          image = image,
-          portMappings = ports,
-          parameters = Nil,
-          privileged = true,
-          network = network
-        )
-      ),
-      instances = 1,
-      cpu = cpu,
-      memory = mem,
-      environmentVariables = Map(),
-      command = command,
-      labels = Map("io.rancher.scheduler.global" -> "true")
-    )
-  }
+  //  private def synchronize() = {
+  //    val actor = IoC.actorFor[ContainerDriverActor]
+  //    actor ? RetrieveDockerApp(id) map {
+  //      case Some(_) ⇒
+  //      case None    ⇒ actor ! DeployDockerApp(request, update = false)
+  //      case _       ⇒
+  //    }
+  //  }
+  //
+  //  private def request = {
+  //    DockerApp(
+  //      id = id,
+  //      container = Option(
+  //        Docker(
+  //          image = image,
+  //          portMappings = ports,
+  //          parameters = Nil,
+  //          privileged = true,
+  //          network = network
+  //        )
+  //      ),
+  //      instances = 1,
+  //      cpu = cpu,
+  //      memory = mem,
+  //      environmentVariables = Map(),
+  //      command = command,
+  //      labels = Map("io.rancher.scheduler.global" -> "true")
+  //    )
+  //  }
 }
