47898091cc4ff9491c621421f65294992e87b49c
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2016-09-07 11:28:06 +0200

    fixing build script

--- .travis-ci-publish.sh
+++ .travis-ci-publish.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-function publish() {
+set -e
+
+function publish {
   # Check bintray credentials
   : ${BINTRAY_USER:?"No BINTRAY_USER set"}
   : ${BINTRAY_API_KEY:?"No BINTRAY_API_KEY set"}
@@ -15,6 +17,8 @@ function publish() {
   : ${SOURCEPATH:?"Not set"}
   : ${VERSION:?"Not set"}
 
+  echo "Bintray upload: ${DISTRIBUTABLE}"
+
   curl -v -T $SOURCEPATH/${DISTRIBUTABLE} \
    -u${BINTRAY_USER}:${BINTRAY_API_KEY} \
    -H "X-Bintray-Package:${PACKAGE}" \
@@ -23,6 +27,11 @@ function publish() {
    https://api.bintray.com/content/magnetic-io/downloads/${PACKAGE}/${DISTRIBUTABLE}
 }
 
+if [ -z "${TRAVIS_BUILD_DIR}" ]; then
+  TRAVIS_BUILD_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+fi
+
+target=${TRAVIS_BUILD_DIR}/target
 version="$( git describe --tags )"
 
 vamp_jar=vamp-${version}.jar
@@ -31,17 +40,17 @@ vamp_cli_zip=vamp-cli-${version}.zip
 
 sbt assembly
 
-cp $(find "${TRAVIS_BUILD_DIR}/bootstrap/target/scala-2.11" -name 'vamp-assembly-*.jar' | sort | tail -1) ${TRAVIS_BUILD_DIR}/${vamp_jar}
+cp $(find "${TRAVIS_BUILD_DIR}/bootstrap/target/scala-2.11" -name 'vamp-assembly-*.jar' | sort | tail -1) ${target}/${vamp_jar}
 
-mkdir -p ${TRAVIS_BUILD_DIR}/brew/brew
-cp $(find "${TRAVIS_BUILD_DIR}/cli/target/scala-2.11" -name 'vamp-cli-*.jar' | sort | tail -1) ${TRAVIS_BUILD_DIR}/brew/${vamp_cli_jar}
+mkdir -p ${target}/brew/brew
+cp $(find "${TRAVIS_BUILD_DIR}/cli/target/scala-2.11" -name 'vamp-cli-*.jar' | sort | tail -1) ${target}/brew/${vamp_cli_jar}
 cp ${TRAVIS_BUILD_DIR}/.travis-ci-brew.sh ${target}/brew/brew/vamp
-cd ${TRAVIS_BUILD_DIR}/brew
+cd ${target}/brew
 zip -r ${vamp_cli_zip} *
 cd ${TRAVIS_BUILD_DIR}
 
-publish vamp ${vamp_jar} ${TRAVIS_BUILD_DIR} ${version}
+publish vamp ${vamp_jar} ${target} ${version}
 
-publish vamp-cli ${vamp_cli_zip} ${TRAVIS_BUILD_DIR}/brew ${version}
+publish vamp-cli ${vamp_cli_zip} ${target}/brew ${version}
 
 sbt publish
--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 reset=`tput sgr0`
