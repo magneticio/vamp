dc5aef5de3bfb2ef26d6edd0c663bfa6e8a720d0
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2016-09-07 20:04:37 +0200

    marathon sse, closes #537

---
+++ container_driver/src/main/scala/io/vamp/container_driver/marathon/MarathonSse.scala
@@ -0,0 +1,38 @@
+package io.vamp.container_driver.marathon
+
+import akka.actor.Actor
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.HttpHeader.ParsingResult
+import akka.http.scaladsl.model.{ HttpHeader, HttpRequest, Uri }
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Source
+import de.heikoseeberger.akkasse.ServerSentEvent
+import io.vamp.common.akka.ExecutionContextProvider
+import org.json4s.native.JsonMethods._
+import org.json4s.{ DefaultFormats, StringInput }
+
+case class MarathonEvent(`type`: String, ids: List[String])
+
+trait MarathonSse {
+  this: Actor with ExecutionContextProvider ⇒
+
+  import de.heikoseeberger.akkasse.EventStreamUnmarshalling._
+
+  def sse(uri: Uri) = {
+
+    implicit val formats = DefaultFormats
+    implicit val actorMaterializer = ActorMaterializer()(context)
+
+    Source.single(HttpRequest(uri = "/v2/events", headers = List(HttpHeader.parse("Accept", "text/event-stream").asInstanceOf[ParsingResult.Ok].header)))
+      .via(Http()(context.system).outgoingConnection(uri.authority.host.address, uri.authority.port))
+      .mapAsync(1)(Unmarshal(_).to[Source[ServerSentEvent, Any]])
+      .runForeach(_.runForeach { event ⇒
+        event.eventType.foreach {
+          self ! MarathonEvent(
+            _, (parse(StringInput(event.data), useBigDecimalForDouble = true) \ "plan" \ "steps" \\ "actions" \ "app").extract[List[String]]
+          )
+        }
+      })
+  }
+}
