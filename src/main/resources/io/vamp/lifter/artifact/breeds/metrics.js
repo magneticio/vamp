48bffd91c6b8c7b9f3eb8f528884dcb485df42f0
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2016-10-05 17:37:57 +0200

    adding console log to health/metrics scripts

--- src/main/resources/io/vamp/lifter/artifact/breeds/metrics.js
+++ src/main/resources/io/vamp/lifter/artifact/breeds/metrics.js
@@ -9,19 +9,24 @@ var metrics = new vamp.Metrics(api);
 var period = 5;  // seconds
 var window = 30; // seconds
 
+function publish(tags, metrics) {
+  console.log('metrics: [' + JSON.stringify(tags) + '] - ' + metrics);
+  api.event(tags, metrics, 'metrics');
+}
+
 var run = function () {
 
   api.gateways().each(function (gateway) {
 
     metrics.average({ft: gateway.lookup_name}, 'Tt', window).each(function (response) {
-      api.event(['gateways:' + gateway.name, 'gateway', 'metrics:rate'], response.rate, 'metrics');
-      api.event(['gateways:' + gateway.name, 'gateway', 'metrics:responseTime'], response.average, 'metrics');
+      publish(['gateways:' + gateway.name, 'gateway', 'metrics:rate'], response.rate);
+      publish(['gateways:' + gateway.name, 'gateway', 'metrics:responseTime'], response.average);
     });
 
     api.namify(gateway.routes).each(function (route) {
       metrics.average({ft: route.lookup_name}, 'Tt', window).each(function (response) {
-        api.event(['gateways:' + gateway.name, 'routes:' + route.name, 'route', 'metrics:rate'], response.rate, 'metrics');
-        api.event(['gateways:' + gateway.name, 'routes:' + route.name, 'route', 'metrics:responseTime'], response.average, 'metrics');
+        publish(['gateways:' + gateway.name, 'routes:' + route.name, 'route', 'metrics:rate'], response.rate);
+        publish(['gateways:' + gateway.name, 'routes:' + route.name, 'route', 'metrics:responseTime'], response.average);
       });
     });
   });
