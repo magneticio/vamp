332678623481480c1829dc614372d7e5c4582f3d
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2016-11-04 18:45:19 +0100

    workflow period and timeout, closes #813

--- src/main/resources/io/vamp/lifter/artifact/breeds/metrics.js
+++ src/main/resources/io/vamp/lifter/artifact/breeds/metrics.js
@@ -6,32 +6,24 @@ var vamp = require('vamp-node-client');
 var api = new vamp.Api();
 var metrics = new vamp.Metrics(api);
 
-var period = 5;  // seconds
 var window = 30; // seconds
 
 function publish(tags, metrics) {
-  console.log('metrics: [' + JSON.stringify(tags) + '] - ' + metrics);
+  api.log('metrics: [' + JSON.stringify(tags) + '] - ' + metrics);
   api.event(tags, metrics, 'metrics');
 }
 
-var run = function () {
+api.gateways().each(function (gateway) {
 
-  api.gateways().each(function (gateway) {
+  metrics.average({ft: gateway.lookup_name}, 'Tt', window).each(function (response) {
+    publish(['gateways:' + gateway.name, 'gateway', 'metrics:rate'], response.rate);
+    publish(['gateways:' + gateway.name, 'gateway', 'metrics:responseTime'], response.average);
+  });
 
-    metrics.average({ft: gateway.lookup_name}, 'Tt', window).each(function (response) {
-      publish(['gateways:' + gateway.name, 'gateway', 'metrics:rate'], response.rate);
-      publish(['gateways:' + gateway.name, 'gateway', 'metrics:responseTime'], response.average);
-    });
-
-    api.namify(gateway.routes).each(function (route) {
-      metrics.average({ft: route.lookup_name}, 'Tt', window).each(function (response) {
-        publish(['gateways:' + gateway.name, 'routes:' + route.name, 'route', 'metrics:rate'], response.rate);
-        publish(['gateways:' + gateway.name, 'routes:' + route.name, 'route', 'metrics:responseTime'], response.average);
-      });
+  api.namify(gateway.routes).each(function (route) {
+    metrics.average({ft: route.lookup_name}, 'Tt', window).each(function (response) {
+      publish(['gateways:' + gateway.name, 'routes:' + route.name, 'route', 'metrics:rate'], response.rate);
+      publish(['gateways:' + gateway.name, 'routes:' + route.name, 'route', 'metrics:responseTime'], response.average);
     });
   });
-};
-
-run();
-
-setInterval(run, period * 1000);
+});
