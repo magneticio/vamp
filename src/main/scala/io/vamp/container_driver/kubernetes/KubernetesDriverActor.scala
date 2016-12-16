88600d64990be7f2b30f4493d9d1155b9080c1c4
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2016-12-16 20:47:35 +0100

    k8s service (gateway) name label

--- src/main/scala/io/vamp/container_driver/kubernetes/KubernetesDriverActor.scala
+++ src/main/scala/io/vamp/container_driver/kubernetes/KubernetesDriverActor.scala
@@ -130,7 +130,7 @@ class KubernetesDriverActor extends ContainerDriverActor with KubernetesContaine
           gateway ⇒ !items.exists { case (l, _) ⇒ l == gateway.lookupName }
         } map { gateway ⇒
           val ports = KubernetesServicePort("port", "TCP", gateway.port.number, gateway.port.number) :: Nil
-          createService(gateway.name, serviceType, vampGatewayAgentId, ports, update = false, gatewayService ++ Map("name" → gateway.name, Lookup.entry → gateway.lookupName))
+          createService(gateway.name, serviceType, vampGatewayAgentId, ports, update = false, gatewayService ++ Map("gateway" → gateway.name, Lookup.entry → gateway.lookupName))
         }
 
         Future.sequence(created ++ deleted)
