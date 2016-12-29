acc88339f44d416d258d60dac3870cb623effef5
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2016-12-29 21:05:33 +0100

    using spi for all

--- src/main/scala/io/vamp/pulse/PulseBootstrap.scala
+++ src/main/scala/io/vamp/pulse/PulseBootstrap.scala
@@ -1,28 +1,19 @@
 package io.vamp.pulse
 
-import akka.actor.{ ActorRef, ActorSystem }
-import io.vamp.common.akka.{ ActorBootstrap, IoC }
+import akka.actor.ActorSystem
+import io.vamp.common.akka.ActorBootstrap
 import io.vamp.common.config.Config
 import io.vamp.pulse.notification.{ PulseNotificationProvider, UnsupportedPulseDriverError }
 
-object PulseBootstrap extends ActorBootstrap with PulseNotificationProvider {
-
+object PulseBootstrap {
   val `type` = Config.string("vamp.pulse.type").toLowerCase
+}
 
-  def createActors(implicit actorSystem: ActorSystem): List[ActorRef] = {
+class PulseBootstrap extends ActorBootstrap with PulseNotificationProvider {
 
-    val pulseActor = `type` match {
-      case "no-store" ⇒
-        IoC.alias[PulseActor, NoStorePulseActor]
-        IoC.createActor[NoStorePulseActor]
-
-      case "elasticsearch" ⇒
-        IoC.alias[PulseActor, ElasticsearchPulseActor]
-        IoC.createActor[ElasticsearchPulseActor]
-
-      case _ ⇒ throwException(UnsupportedPulseDriverError(`type`))
-    }
-
-    pulseActor :: Nil
+  def createActors(implicit actorSystem: ActorSystem) = {
+    alias[PulseActor](PulseBootstrap.`type`, (`type`: String) ⇒ {
+      throwException(UnsupportedPulseDriverError(`type`))
+    }) :: Nil
   }
 }
