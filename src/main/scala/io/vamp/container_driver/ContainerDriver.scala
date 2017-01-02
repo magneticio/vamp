2af017b6c30c0cbe0ce35cffec9b2d86c0ca1ad2
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2017-01-02 17:01:17 +0100

    removed redundant HAProxy configuration, closes #763

--- src/main/scala/io/vamp/container_driver/ContainerDriver.scala
+++ src/main/scala/io/vamp/container_driver/ContainerDriver.scala
@@ -4,7 +4,7 @@ import io.vamp.common.akka.ExecutionContextProvider
 import io.vamp.common.config.Config
 import io.vamp.common.http.HttpClient
 import io.vamp.common.notification.NotificationProvider
-import io.vamp.container_driver.notification.{ContainerDriverNotificationProvider, UndefinedDockerImage, UnsupportedDeployableType}
+import io.vamp.container_driver.notification.{ ContainerDriverNotificationProvider, UndefinedDockerImage, UnsupportedDeployableType }
 import io.vamp.model.artifact._
 import io.vamp.model.resolver.DeploymentTraitResolver
 
