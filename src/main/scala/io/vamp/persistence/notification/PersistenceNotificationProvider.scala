2200f465fcab885e5d372f89ab50a7e5892938b3
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2017-01-25 20:43:00 +0100

    import from vamp repo

--- src/main/scala/io/vamp/persistence/notification/PersistenceNotificationProvider.scala
+++
@@ -1,5 +0,0 @@
-package io.vamp.persistence.notification
-
-import io.vamp.common.notification.{ DefaultPackageMessageResolverProvider, LoggingNotificationProvider }
-
-trait PersistenceNotificationProvider extends LoggingNotificationProvider with DefaultPackageMessageResolverProvider
