b15bb5b2bdf7c4f6b47f7e45af9d6af200f3c8aa
ThomasMeijers <thomasmeijers@live.nl>
2017-02-14 10:56:51 +0100

    Fix typo in comment

--- src/main/scala/io/vamp/container_driver/marathon/ReadApps.scala
+++ src/main/scala/io/vamp/container_driver/marathon/ReadApps.scala
@@ -7,7 +7,7 @@ case class AppsResponse(apps: List[App])
 case class App(id: String, instances: Int, cpus: Double, mem: Double, tasks: List[Task], healthChecks: List[MarathonHealthCheck])
 
 /**
-  * A class to compare the app and marathon app for checking wheter Marathon needs to be updated based on config settings
+  * A class to compare the app and marathon app for checking whether Marathon needs to be updated based on config settings
   */
 case class ComparableApp private (id: String, cpus: Double, mem: Double, healthChecks: List[MarathonHealthCheck])
 
