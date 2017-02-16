752c93f949f1d642923a857d33a1960ceeaa86b7
ThomasMeijers <thomasmeijers@live.nl>
2017-02-16 16:27:03 +0100

    Change port in HealthCheck to reference only

--- src/main/scala/io/vamp/container_driver/marathon/MarathonApp.scala
+++ src/main/scala/io/vamp/container_driver/marathon/MarathonApp.scala
@@ -34,37 +34,21 @@ object MarathonHealthCheck {
 
   /** Transforms a HealthCheck to a Marathon specific HealthCheck */
   def apply(ports: List[Port], healthCheck: HealthCheck): MarathonHealthCheck = {
-    val portOrIndex: Either[Int, Int] = Try(Left(healthCheck.port.toInt))
-      .getOrElse { Right(
-       ports
-         .zipWithIndex
-         .find { case (p, i) => p.name.contains(healthCheck.port) || p.alias.contains(healthCheck.port) }
-         .get // Able to get due to validation
-         ._2
-      )}
+    val index: Int = ports
+      .zipWithIndex
+      .find { case (p, i) => p.name.contains(healthCheck.port) || p.alias.contains(healthCheck.port) }
+      .get // Able to get due to validation
+      ._2
 
-    portOrIndex match {
-      case Left(port)   =>
-        MarathonHealthCheck(
-          healthCheck.path,
-          Some(port),
-          None,
-          healthCheck.protocol,
-          healthCheck.initialDelay.value,
-          healthCheck.interval.value,
-          healthCheck.timeout.value,
-          healthCheck.failures)
-      case Right(index) =>
-        MarathonHealthCheck(
-          healthCheck.path,
-          None,
-          Some(index),
-          healthCheck.protocol,
-          healthCheck.initialDelay.value,
-          healthCheck.interval.value,
-          healthCheck.timeout.value,
-          healthCheck.failures)
-    }
+    MarathonHealthCheck(
+      healthCheck.path,
+      None,
+      Some(index),
+      healthCheck.protocol,
+      healthCheck.initialDelay.value,
+      healthCheck.interval.value,
+      healthCheck.timeout.value,
+      healthCheck.failures)
   }
 
   /**
