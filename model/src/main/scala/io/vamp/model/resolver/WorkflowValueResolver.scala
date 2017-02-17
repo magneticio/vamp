b6b446ed3132dd944bea9c1d9d81f65b5d4aa155
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2017-02-17 13:18:45 +0100

    2 phase workflow env resolver #906

--- model/src/main/scala/io/vamp/model/resolver/WorkflowValueResolver.scala
+++ model/src/main/scala/io/vamp/model/resolver/WorkflowValueResolver.scala
@@ -7,14 +7,20 @@ trait WorkflowValueResolver extends ValueResolver {
   this: NotificationProvider ⇒
 
   def resolveEnvironmentVariable(workflow: Workflow): EnvironmentVariable ⇒ EnvironmentVariable = { env ⇒
-    env.copy(interpolated = env.value.map(value ⇒ resolve(value, valueFor(workflow))))
+    env.copy(interpolated = env.value.map { value ⇒
+      resolve(
+        resolve(
+          value,
+          valueFor(workflow, GlobalValueResolver.valueForReference)
+        ),
+        valueFor(workflow, valueForWorkflow(workflow: Workflow))
+      )
+    })
   }
 
-  private def valueFor(workflow: Workflow)(reference: ValueReference): String = (
-    valueForWorkflow(workflow)
-    orElse GlobalValueResolver.valueForReference
-    orElse PartialFunction[ValueReference, String] { _ ⇒ "" }
-  )(reference)
+  private def valueFor(workflow: Workflow, resolver: PartialFunction[ValueReference, String])(reference: ValueReference): String = {
+    (resolver orElse PartialFunction[ValueReference, String] { _ ⇒ "" })(reference)
+  }
 
   private def valueForWorkflow(workflow: Workflow): PartialFunction[ValueReference, String] = {
     case LocalReference("workflow")           ⇒ workflow.name
