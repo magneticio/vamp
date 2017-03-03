1fe9d11af5391bddc9ecc15bdb3d8dc30048412e
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2017-03-03 20:13:13 +0100

    namespace update

--- src/main/scala/io/vamp/container_driver/kubernetes/KubernetesDeployment.scala
+++ src/main/scala/io/vamp/container_driver/kubernetes/KubernetesDeployment.scala
@@ -16,11 +16,11 @@ trait KubernetesDeployment extends KubernetesArtifact {
 
   private val workflowIdLabel = "workflow"
 
-  private lazy val podUrl = s"$apiUrl/api/v1/namespaces/${namespace.id}/pods"
+  private lazy val podUrl = s"$apiUrl/api/v1/namespaces/${namespace.name}/pods"
 
-  private lazy val replicaSetUrl = s"$apiUrl/apis/extensions/v1beta1/namespaces/${namespace.id}/replicasets"
+  private lazy val replicaSetUrl = s"$apiUrl/apis/extensions/v1beta1/namespaces/${namespace.name}/replicasets"
 
-  private lazy val deploymentUrl = s"$apiUrl/apis/extensions/v1beta1/namespaces/${namespace.id}/deployments"
+  private lazy val deploymentUrl = s"$apiUrl/apis/extensions/v1beta1/namespaces/${namespace.name}/deployments"
 
   override protected def supportedDeployableTypes = RktDeployableType :: DockerDeployableType :: Nil
 
