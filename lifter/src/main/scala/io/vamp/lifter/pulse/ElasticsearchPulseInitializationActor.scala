affcd83b190087195c965e4fa4ce4e15fc2f0a48
ThomasMeijers <thomasmeijers@live.nl>
2017-07-11 16:52:26 +0200

    Add type annotations to PulseInitalizationActor

--- lifter/src/main/scala/io/vamp/lifter/pulse/ElasticsearchPulseInitializationActor.scala
+++ lifter/src/main/scala/io/vamp/lifter/pulse/ElasticsearchPulseInitializationActor.scala
@@ -10,13 +10,13 @@ import scala.io.Source
 
 class ElasticsearchPulseInitializationActor extends ElasticsearchPulseEvent with NamespaceValueResolver with ElasticsearchInitializationActor with LifterNotificationProvider {
 
-  lazy val indexName = resolveWithNamespace(ElasticsearchPulseActor.indexName(), lookup = true)
+  lazy val indexName: String = resolveWithNamespace(ElasticsearchPulseActor.indexName(), lookup = true)
 
-  lazy val indexTimeFormat = ElasticsearchPulseActor.indexTimeFormat()
+  lazy val indexTimeFormat: Map[String, String] = ElasticsearchPulseActor.indexTimeFormat()
 
-  lazy val elasticsearchUrl = ElasticsearchPulseActor.elasticsearchUrl()
+  lazy val elasticsearchUrl: String = ElasticsearchPulseActor.elasticsearchUrl()
 
-  override lazy val templates = {
+  override lazy val templates: List[TemplateDefinition] = {
     def load(name: String) = Source.fromInputStream(getClass.getResourceAsStream(s"$name.json")).mkString.replace("$NAME", indexName)
 
     List("template", "template-event").map(template ⇒ TemplateDefinition(s"$indexName-$template", load(template)))
