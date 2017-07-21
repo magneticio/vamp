b649c5831db81e0f2abc73d71bbe97a538108ea7
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2017-07-22 00:07:36 +0200

    optional snake case serialization

--- model/src/main/scala/io/vamp/model/serialization/UpperCaseEnumSerializer.scala
+++ model/src/main/scala/io/vamp/model/serialization/UpperCaseEnumSerializer.scala
@@ -1,16 +1,17 @@
 package io.vamp.model.serialization
 
+import io.vamp.common.util.TextUtil
 import org.json4s.{ Formats, JValue, JsonDSL, Serializer }
 
 import scala.reflect.ClassTag
 
-class UpperCaseEnumSerializer[E <: Enumeration: ClassTag](enum: E) extends Serializer[E#Value] {
+class UpperCaseEnumSerializer[E <: Enumeration: ClassTag](enum: E, snakeCase: Boolean = false) extends Serializer[E#Value] {
 
   import JsonDSL._
 
   def deserialize(implicit format: Formats) = throw new NotImplementedError
 
   def serialize(implicit format: Formats): PartialFunction[Any, JValue] = {
-    case i: E#Value ⇒ i.toString.toUpperCase
+    case i: E#Value ⇒ if (snakeCase) TextUtil.toSnakeCase(i.toString, dash = false).toUpperCase else i.toString.toUpperCase
   }
 }
