b69078e748cfe1a766b4fbf09faaa318d11f8e75
mihaiSimu <mihai.simu@busymachines.com>
2017-11-22 16:18:56 +0200

    Allowing Pagination for artifact-searches at DAO level

--- persistence/src/main/scala/io/vamp/persistence/refactor/dao/EsDao.scala
+++ persistence/src/main/scala/io/vamp/persistence/refactor/dao/EsDao.scala
@@ -97,11 +97,19 @@ class EsDao(val namespace: Namespace, elasticSearchHostAndPort: String, elasticS
     } yield ()
   }
 
-  def getAll[T](s: SerializationSpecifier[T]): Future[List[T]] = {
+  def getAll[T](s: SerializationSpecifier[T], fromAndSize: Option[(Int, Int)] = None): Future[List[T]] = {
     implicit val sSpecifier: SerializationSpecifier[T] = s
     for {
       numberOfObjects ← esClient.execute(search(indexName) types (s.typeName) size 0)
-      allObjects ← esClient.execute(search(indexName) types (s.typeName) size numberOfObjects.totalHits.toInt)
+      allObjects ← esClient.execute(search(indexName) types (s.typeName) from (
+        fromAndSize match {
+          case None => 0
+          case Some((f, _)) => f
+        }) size (
+        fromAndSize match {
+          case None => numberOfObjects.totalHits.toInt
+          case Some((_, t)) => t
+        }))
     } yield {
       val responseHits = allObjects.original.getHits().getHits()
       responseHits.map(s => interpretAsObject(s.getSourceAsString)).toList
--- persistence/src/test/scala/io/vamp/persistence/refactor/ESPersistenceTest_Gateway.scala
+++ persistence/src/test/scala/io/vamp/persistence/refactor/ESPersistenceTest_Gateway.scala
@@ -11,30 +11,40 @@ import org.scalatest.{BeforeAndAfterEach, Matchers, fixture}
  */
 class ESPersistenceTest_Gateway extends fixture.FlatSpec with Matchers with UseElasticSearchForTesting with BeforeAndAfterEach with VampJsonFormats {
 
+  val exampleGateway = Gateway(name = "gateway_1",
+    metadata = RootAnyMap.empty,
+    port = Port(name = "port01", alias = Some("portAlias01"), value = Some("value01"), number = 1, `type` = Port.Type.Http),
+    service = Some(GatewayService(host = "localhost01", port = Port(name = "port01_01", alias = Some("portAlias01_01"), value = Some("value01_01"), number = 11, `type` = Port.Type.Tcp))),
+    sticky = Some(Gateway.Sticky.Route), virtualHosts = List("h001", "h002", "h003"),
+    routes = List[Route](
+      DefaultRoute(name = "defaultReout01", metadata = RootAnyMap.empty, path = GatewayPath(source = "source01", segments = List[String]("seg01", "seg02")),
+        weight = Some(Percentage(value = 66)), condition = Some(DefaultCondition(name = "cond001", metadata = RootAnyMap.empty, definition = "def001")), conditionStrength = Some(Percentage(value = 55)),
+        rewrites = List[Rewrite](
+          RewriteReference("rewriteName01"),
+          PathRewrite(name = "pathRewrite01", path = "somePath", condition = "someCondition")
+        ),
+        balance = Some("balance001"),
+        targets = List[RouteTarget](ExternalRouteTarget("sumeUrl_007", metadata = RootAnyMap.empty), InternalRouteTarget(name = "internalRouteTarget", host = Some("host98"), port = 3306))
+      ),
+      RouteReference(name = "routeReference01", path = GatewayPath(source = "source02", segments = List[String]("seg03", "seg04")))
+    ),
+    deployed = false)
+
   behavior of "EsDao"
   it should "Correctly Persist Gateway objects" in { implicit namespace: Namespace ⇒
-    val gateway1 = Gateway(name = "gateway_1",
-      metadata = RootAnyMap.empty,
-      port = Port(name = "port01", alias = Some("portAlias01"), value = Some("value01"), number = 1, `type` = Port.Type.Http),
-      service = Some(GatewayService(host = "localhost01", port = Port(name = "port01_01", alias = Some("portAlias01_01"), value = Some("value01_01"), number = 11, `type` = Port.Type.Tcp))),
-      sticky = Some(Gateway.Sticky.Route), virtualHosts = List("h001", "h002", "h003"),
-      routes = List[Route](
-        DefaultRoute(name = "defaultReout01", metadata = RootAnyMap.empty, path = GatewayPath(source = "source01", segments = List[String]("seg01", "seg02")),
-            weight = Some(Percentage(value = 66)), condition = Some(DefaultCondition(name = "cond001", metadata = RootAnyMap.empty, definition = "def001")), conditionStrength = Some(Percentage(value = 55)),
-            rewrites = List[Rewrite](
-                RewriteReference("rewriteName01"),
-                PathRewrite(name = "pathRewrite01", path = "somePath", condition = "someCondition")
-            ),
-            balance = Some("balance001"),
-            targets = List[RouteTarget](ExternalRouteTarget("sumeUrl_007", metadata = RootAnyMap.empty), InternalRouteTarget(name = "internalRouteTarget", host = Some("host98"), port = 3306))
-        ),
-        RouteReference(name = "routeReference01", path = GatewayPath(source = "source02", segments = List[String]("seg03", "seg04")))
-      ),
-      deployed = false)
+    val gateway1 = exampleGateway
 
     // Create and retrieve; See that the object is there
     val gateway1Id = simpleAwait(VampPersistence().create(gateway1))
     assert(simpleAwait(VampPersistence().read(gateway1Id)) == gateway1)
   }
 
+
+  behavior of "JsonFormats"
+  it should "currectly marshall and unmarshall lists of gateways" in {p =>
+
+    val listOfGateways = List(exampleGateway, exampleGateway.copy(name = "modifiedName"))
+    val marshalledListOfGateways = marshallList[Gateway](listOfGateways)
+    assert(unMarshallList[Gateway](marshalledListOfGateways) == listOfGateways)
+  }
 }
