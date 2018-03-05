716e2951b84fd2e21941e02a5588822f30edf1d4
Dragoslav Pavkovic <dragoslav.pavkovic@gmail.com>
2018-03-05 14:55:51 +0100

    making public access to scheduler route string (scala api)

--- http_api/src/main/scala/io/vamp/http_api/SchedulerRoute.scala
+++ http_api/src/main/scala/io/vamp/http_api/SchedulerRoute.scala
@@ -8,11 +8,15 @@ import io.vamp.common.akka._
 import io.vamp.common.http.HttpApiDirectives
 import io.vamp.operation.controller.SchedulerController
 
+object SchedulerRoute {
+  val path: String = "scheduler"
+}
+
 trait SchedulerRoute extends AbstractRoute with SchedulerController with ExecutionContextProvider {
   this: HttpApiDirectives ⇒
 
   def routingRoutes(implicit namespace: Namespace, timeout: Timeout): Route = get {
-    path("scheduler" / "routing") {
+    path(SchedulerRoute.path / "routing") {
       pageAndPerPage() { (page, perPage) ⇒
         parameters('selector.?) { selector ⇒
           onSuccess(routing(selector)(page, perPage)) {
