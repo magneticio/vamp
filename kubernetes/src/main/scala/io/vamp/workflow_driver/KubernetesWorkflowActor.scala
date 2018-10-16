be09831aa0c0f417c9f0f607f00f8e735165aabd
Alessandro Valcepina <alessandro@magnetic.io>
2018-10-16 13:04:58 +0200

    Another attempt at fixing workflow status

--- kubernetes/src/main/scala/io/vamp/workflow_driver/KubernetesWorkflowActor.scala
+++ kubernetes/src/main/scala/io/vamp/workflow_driver/KubernetesWorkflowActor.scala
@@ -30,12 +30,12 @@ class KubernetesWorkflowActor extends DaemonWorkflowDriver with WorkflowValueRes
   override protected lazy val driverActor: ActorRef = IoC.actorFor[KubernetesDriverActor]
 
   protected override def request: PartialFunction[Workflow, Unit] = {
-    case workflow if workflow.schedule.isInstanceOf[EventSchedule] ⇒
+    case workflow ⇒
       logger.info("KubernetesWorkflowActor - Workflow schedule is an instance of EventSchedule - {}", workflow.toString)
       logger.info("KubernetesWorkflowActor - asking for percolator {}", WorkflowDriverActor.percolator(workflow))
 
       IoC.actorFor[PulseActor] ? GetPercolator(WorkflowDriverActor.percolator(workflow)) map {
-        case Some(_)  ⇒
+        case Some(_) if runnable(workflow) ⇒
           if (workflow.instances.isEmpty) {
             logger.info(s"KubernetesWorkflowActor - workflow.instances.isEmpty : ${workflow.instances.isEmpty}")
             IoC.actorFor[PersistenceActor] ! PersistenceActor.UpdateWorkflowInstances(workflow, Instance(workflow.name, "", Map(), deployed = true) :: Nil)
