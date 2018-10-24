59e773f8908a48c20c7c0442d0e51bd2a922259b
Alessandro Valcepina <alessandro@magnetic.io>
2018-10-24 11:43:12 +0200

    More logs on service deletion

--- kubernetes/src/main/scala/io/vamp/container_driver/kubernetes/K8sCache.scala
+++ kubernetes/src/main/scala/io/vamp/container_driver/kubernetes/K8sCache.scala
@@ -39,28 +39,41 @@ class K8sCache(config: K8sCacheConfig, val namespace: Namespace) {
 
   def writeWithCache(operation: String, kind: String, name: String, request: () ⇒ Any): Unit =
     Try(requestWithCache[Any](read = false, operation, id(kind, name, read = false), request)).recover {
-      case e: ApiException if e.getCode == 409 ⇒ // conflict, previous operation still in progress
-      case e                                   ⇒ logger.warn(e.getMessage)
+      case e: ApiException if e.getCode == 409 ⇒
+        logger.warn("K8sCache - Conflict, previous operation still in progress - {}", e.getMessage)
+      // conflict, previous operation still in progress
+      case e ⇒
+        logger.error("K8sCache - error while writing with cache", e)
     }
 
   private def requestWithCache[T](read: Boolean, operation: String, id: String, request: () ⇒ T): (String, T) = {
     cache.get[(String, Either[T, Exception])](id) match {
       case Some((op, response)) if op == operation ⇒
-        logger.debug(s"cache get: $id")
+        logger.debug(s"K8sCache - Getting response from cache for $id on operation $operation")
         response match {
-          case Left(r)  ⇒ op → r
-          case Right(e) ⇒ throw e
+          case Left(r) ⇒ {
+            logger.info("K8sCache - Returning from cache for {} on operation {} response was {}", id, operation, response)
+            op → r
+          }
+          case Right(e) ⇒ {
+            logger.error(s"K8sCache - Error while retrieving from cache for $id on operation $operation", e)
+            throw e
+          }
         }
       case _ ⇒
         try {
+          logger.info("K8sCache - Sending request for {} on operation {}", id, operation)
           val response = request()
+          logger.info("K8sCache - Request sent for {} on operation {} response was {}", id, operation, response)
           val ttl = if (read) config.readTimeToLivePeriod else config.writeTimeToLivePeriod
-          logger.info(s"cache put [${ttl.toSeconds}s]: $id")
+          logger.info(s"cK8sCache - cache put [${ttl.toSeconds}s]: $id on operation $operation")
           cache.put[(String, Either[T, Exception])](id, operation → Left(response), ttl)
+          logger.info("K8sCache - Returning from request for {} on operation {}", id, operation)
           operation → response
         }
         catch {
           case e: Exception ⇒
+            logger.error(s"K8sCache - Error while running request for $id on operation $operation", e)
             val ttl = config.failureTimeToLivePeriod
             logger.info(s"cache put [${ttl.toSeconds}s]: $id")
             cache.put[(String, Either[T, Exception])](id, operation → Right(e), ttl)
