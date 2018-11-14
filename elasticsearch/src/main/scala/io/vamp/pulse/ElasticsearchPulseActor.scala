da5b94249139ab79555eb667c134283366140b27
berkgokden <berkgokden@gmail.com>
2018-11-14 16:52:38 +0100

    adding index type name to get events

--- elasticsearch/src/main/scala/io/vamp/pulse/ElasticsearchPulseActor.scala
+++ elasticsearch/src/main/scala/io/vamp/pulse/ElasticsearchPulseActor.scala
@@ -136,6 +136,9 @@ class ElasticsearchPulseActor extends ElasticsearchPulseEvent with NamespaceValu
     implicit val formats: Formats = SerializationFormat(OffsetDateTimeSerializer, new EnumNameSerializer(Aggregator))
     val (p, pp) = OffsetEnvelope.normalize(page, perPage, EventRequestEnvelope.maxPerPage)
 
+    val (indexName, typeName) = indexTypeName(query.`type`.getOrElse(Event.defaultType)) // TODO: testing index type name
+    logger.info("Get Events called for index {}", indexName)
+
     es.search[ElasticsearchSearchResponse](indexName, constructSearch(query, p, pp)) map {
       case ElasticsearchSearchResponse(hits) ⇒
         val events = hits.hits.flatMap { hit ⇒
