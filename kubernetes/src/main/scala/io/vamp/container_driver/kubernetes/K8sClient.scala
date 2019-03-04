f54167f90252ac7a5600e29f74fed51c5622c42b
berkgokden <berkgokden@gmail.com>
2019-03-04 16:01:21 +0100

    Convert pem files to pkcs12

--- kubernetes/src/main/scala/io/vamp/container_driver/kubernetes/K8sClient.scala
+++ kubernetes/src/main/scala/io/vamp/container_driver/kubernetes/K8sClient.scala
@@ -1,6 +1,6 @@
 package io.vamp.container_driver.kubernetes
 
-import java.io.{FileInputStream, IOException, InputStream}
+import java.io._
 import java.security.cert.{Certificate, CertificateFactory}
 import java.security.{KeyStore, SecureRandom}
 import java.util
@@ -12,6 +12,11 @@ import io.kubernetes.client.ApiClient
 import io.kubernetes.client.apis.{ApisApi, BatchV1Api, CoreV1Api, ExtensionsV1beta1Api}
 import io.vamp.common.Namespace
 import javax.net.ssl.{KeyManagerFactory, SSLContext, TrustManagerFactory}
+import org.bouncycastle.cert.X509CertificateHolder
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter
+import org.bouncycastle.jce.provider.BouncyCastleProvider
+import org.bouncycastle.openssl.{PEMKeyPair, PEMParser}
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter
 import org.slf4j.LoggerFactory
 
 import scala.collection.mutable
@@ -68,22 +73,66 @@ class K8sClient(val config: K8sClientConfig)(implicit system: ActorSystem) {
     if (config.username.nonEmpty) client.setUsername(config.username)
     if (config.password.nonEmpty) client.setPassword(config.password)
     if (config.serverCaCert.nonEmpty) client.setSslCaCert(new FileInputStream(config.serverCaCert))
-    if (config.clientCert.nonEmpty) {
+    if (config.clientCert.nonEmpty && config.privateKey.nonEmpty) {
       // client.getHttpClient
-      setCert(client, config.clientCert)
+      setCert(client, config.privateKey, config.clientCert)
 
     }
     client.setVerifyingSsl(config.tlsCheck)
   }
 
+  /**
+    * Pem keys as private key and server certificates used as input in many applications
+    * but akka requires PKCS12 type keys for https, so this method is needed for conversion
+    * TODO: check keystore if this method is not actually needed.
+    * @param keyString private-key
+    * @param cerString server-certificate
+    * @param password password for keystore default is change me
+    * @return PKCS12 file as byte array
+    */
+  def convertPEMToPKCS12(keyString: String, cerString: String, password: String): Array[Byte] = { // Get the private key
+    var reader = new StringReader(keyString)
+    var pem = new PEMParser(reader)
+    val pemKeyPair = pem.readObject.asInstanceOf[PEMKeyPair]
+    val provider = new BouncyCastleProvider()
+    val jcaPEMKeyConverter = new JcaPEMKeyConverter().setProvider(provider)
+    val keyPair = jcaPEMKeyConverter.getKeyPair(pemKeyPair)
+    val key = keyPair.getPrivate
+    pem.close()
+    reader.close()
+    // Get the certificate
+    reader = new StringReader(cerString)
+    pem = new PEMParser(reader)
+    val certHolder = pem.readObject.asInstanceOf[X509CertificateHolder]
+    val X509Certificate = new JcaX509CertificateConverter().setProvider(provider).getCertificate(certHolder)
+    pem.close()
+    reader.close()
+    // Put them into a PKCS12 keystore and write it to a byte[]
+    val bos = new ByteArrayOutputStream()
+    val ks: KeyStore = KeyStore.getInstance("PKCS12")
+    ks.load(null)
+    val certs = new Array[java.security.cert.Certificate](1)
+    certs(0) = X509Certificate
+    ks.setKeyEntry("alias", key.asInstanceOf[java.security.Key], password.toCharArray, certs )
+    ks.store(bos, password.toCharArray)
+    bos.close
+    bos.toByteArray
+  }
 
-  private def setCert(apiClient: ApiClient, certfilepath: String) : Unit = {
+
+  private def setCert(apiClient: ApiClient, keyfilepath: String, certfilepath: String) : Unit = {
+
+    val keyfileAsString = scala.io.Source.fromFile(keyfilepath).mkString
+    val certfileAsString = scala.io.Source.fromFile(certfilepath).mkString
+
+    val pkcs12certFileAsByteArray = convertPEMToPKCS12(keyfileAsString, certfileAsString, "change me")
+
+    val keyInput = new ByteArrayInputStream(pkcs12certFileAsByteArray)
     import java.security.KeyStore
     val password: Array[Char] = null
     val keyManagerFactory: KeyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm)
-    val keyStore: KeyStore = KeyStore.getInstance(KeyStore.getDefaultType)
+    val keyStore: KeyStore = KeyStore.getInstance("PKCS12")
 
-    val keyInput: InputStream = new FileInputStream(certfilepath)
     keyStore.load(keyInput, password)
     keyInput.close()
     keyManagerFactory.init(keyStore, password)
